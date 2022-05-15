338927a1013f76ffd29d354e6bedccec881ea28a
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-05-15 18:49:23 +0900

    Configure packages/stlite-bridge to build

---
+++ packages/stlite-bridge/src/index.ts
@@ -0,0 +1,5 @@
+export class WorkerConnection {
+  /* TODO: Be the replacement of WebsocketConnection (https://github.com/streamlit/streamlit/blob/develop/frontend/src/lib/WebsocketConnection.tsx) */
+
+
+}

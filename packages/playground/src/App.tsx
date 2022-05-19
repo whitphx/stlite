7ff17cf8f1492e6d7083761ae575a025198a7ff6
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-05-19 23:56:01 +0900

    Set the default script on the playground

--- packages/playground/src/App.tsx
+++ packages/playground/src/App.tsx
@@ -8,8 +8,13 @@ import { Client as Styletron } from "styletron-engine-atomic";
 import { Provider as StyletronProvider } from "styletron-react";
 const engine = new Styletron({ prefix: "st-" });
 
+const DEFAULT_VALUE = `import streamlit as st
+
+name = st.text_input("Name")
+st.write("Hello ", name or "world")`;
+
 function App() {
-  const [mainScriptData, setMainScriptData] = useState("");
+  const [mainScriptData, setMainScriptData] = useState(DEFAULT_VALUE);
 
   return (
     <>

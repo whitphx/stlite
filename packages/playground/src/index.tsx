081cd13451bf3f5fce0d95bdb555a3193c146c36
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-05-21 13:42:34 +0900

    Remove unnecessary playground/src/index.css

--- packages/playground/src/index.tsx
+++ packages/playground/src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";

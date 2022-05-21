b2e866826b1acbbf95ce7f109f9a50bd72f649ac
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-05-21 13:40:17 +0900

    Update packages/stlite to include the streamlit frontend and the kernel

--- packages/stlite/src/App.tsx
+++ packages/stlite/src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import ThemedApp from "streamlit-browser/src/ThemedApp";
+import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as StyletronProvider } from "styletron-react";
+const engine = new Styletron({ prefix: "st-" });
+
+export interface AppProps {
+  mainScriptData: string;
+}
+function App(props: AppProps) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyletronProvider value={engine}>
+      <ThemedApp stliteMainScriptData={props.mainScriptData} />
+    </StyletronProvider>
   );
 }
 
--- packages/stlite/src/index.tsx
+++ packages/stlite/src/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root") as HTMLElement
-);
+export interface MountOptions {
+  mainScriptData: string;
+  container?: HTMLElement;
+}
+function mount(options: MountOptions) {
+  const container = options.container || document.body;
+  ReactDOM.render(
+    <React.StrictMode>
+      <App mainScriptData={options.mainScriptData} />
+    </React.StrictMode>,
+    container
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Sample:
+mount({
+  mainScriptData: `import streamlit as st
+
+st.write("Hello World")`,
+});
+

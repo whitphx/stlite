25e8e168b44736de188f397751275cc731a9c3cd
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-06-07 00:01:16 +0900

    Implement WebSocketHandler.request.headers for header checkings in server.py in 1.10.0

--- packages/stlite-kernel/py/stlite-tornado/tornado/httputil.py
+++ packages/stlite-kernel/py/stlite-tornado/tornado/httputil.py
@@ -1,2 +1,7 @@
+from typing import Optional
+
+HTTPHeaders = dict  # Very simplified mock just for stlite where only the Streamlit server reads the header object.
+
 class HTTPServerRequest(object):
-    pass
+    def __init__(self, headers: Optional[HTTPHeaders] = None) -> None:
+        self.headers = headers or HTTPHeaders()

370edfc4a1ac264497decc70a54427ee2783a715
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-08-11 20:02:31 +0900

    Move test files

--- packages/stlite-kernel/py/e2etests/tests/conftest.py
+++ packages/stlite-kernel/py/e2etests/tests/conftest.py
@@ -4,12 +4,13 @@ from unittest.mock import Mock
 
 
 HERE = Path(__file__).parent.resolve()
-REPO_ROOT = HERE / "../../.."
+REPO_ROOT = HERE / "../../../.."
+
 STREAMLIT_SUBMODULE = REPO_ROOT / "streamlit"
 STREAMLIT_PYTHONPATH = STREAMLIT_SUBMODULE / "lib"
 sys.path.insert(0, str(STREAMLIT_PYTHONPATH))
 
-TORNADO_SUBMODULE = HERE / "../tornado"
+TORNADO_SUBMODULE = HERE / "../../tornado"
 TORNADO_PYTHONPATH = TORNADO_SUBMODULE
 sys.path.insert(0, str(TORNADO_PYTHONPATH))
 

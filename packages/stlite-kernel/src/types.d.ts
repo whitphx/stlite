9ff4e51dc22a7a5241516edb20b3435a27b6e848
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-08-13 01:04:05 +0900

    Define message types

--- packages/stlite-kernel/src/types.d.ts
+++ packages/stlite-kernel/src/types.d.ts
@@ -1,4 +1,94 @@
+interface HttpRequest {
+  method: "GET" | "POST";
+  path: string;
+  headers: { [key: string]: string };
+  body: ArrayBuffer | string;
+}
+interface HttpResponse {
+  statusCode: number;
+  headers: Map<string, string>;
+  body: Uint8Array;
+}
 interface WorkerInitialData {
   requirements: string[];
   mainScriptData?: string;
 }
+
+/**
+ * Messages to worker from kernel
+ */
+interface MessageToWorkerBase {
+  type: string;
+  data?: unknown;
+}
+interface InitDataMessage extends MessageToWorkerBase {
+  type: "initData";
+  data: WorkerInitialData;
+}
+interface WebSocketConnectMessage extends MessageToWorkerBase {
+  type: "websocket:connect";
+  data: {
+    path: string;
+  };
+}
+interface WebSocketSendMessage extends MessageToWorkerBase {
+  type: "websocket:send";
+  data: {
+    payload: Uint8Array;
+  };
+}
+interface HttpRequestMessage extends MessageToWorkerBase {
+  type: "http:request";
+  data: {
+    httpCommId: number;
+    request: HttpRequest;
+  };
+}
+interface MainScriptSetMessage extends MessageToWorkerBase {
+  type: "mainscript:set";
+  data: {
+    mainScriptData: string;
+  };
+}
+type MessageToWorker =
+  | InitDataMessage
+  | WebSocketConnectMessage
+  | WebSocketSendMessage
+  | HttpRequestMessage
+  | MainScriptSetMessage;
+
+interface StliteWorker extends Worker {
+  postMessage(message: MessageToWorker);
+}
+
+/**
+ * Messages from worker to kernel
+ */
+interface MessageFromWorkerBase {
+  type: string;
+  data?: unknown;
+}
+interface StartEventMessage extends MessageFromWorkerBase {
+  type: "event:start";
+}
+interface LoadedEventMessage extends MessageFromWorkerBase {
+  type: "event:loaded";
+}
+interface WebSocketBackMessage extends MessageFromWorkerBase {
+  type: "websocket:message";
+  data: {
+    payload: Uint8Array;
+  };
+}
+interface HttpResponseMessage extends MessageFromWorkerBase {
+  type: "http:response";
+  data: {
+    httpCommId: number;
+    response: HttpResponse;
+  };
+}
+type MessageFromWorker =
+  | StartEventMessage
+  | LoadedEventMessage
+  | WebSocketBackMessage
+  | HttpResponseMessage;

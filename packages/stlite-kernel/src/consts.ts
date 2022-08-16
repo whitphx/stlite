9bc71aa3f179a420134a671de94ee09ce720af57
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-08-16 17:45:01 +0900

    Update comments

--- packages/stlite-kernel/src/consts.ts
+++ packages/stlite-kernel/src/consts.ts
@@ -1,4 +1,6 @@
-export const DUMMY_BASE_HOSTNAME = "xxx"; // Invalid URL
-export const DUMMY_BASE_PORT = 99999; // Invalid URL
+// The following hostname and port are invalid values for constructing an actual URL
+// so that they should not be accidentally used to refer to and access external resources.
+export const DUMMY_BASE_HOSTNAME = "xxx";
+export const DUMMY_BASE_PORT = 99999;
 export const DUMMY_BASE_HOST =
   DUMMY_BASE_HOSTNAME + ":" + DUMMY_BASE_PORT.toString();

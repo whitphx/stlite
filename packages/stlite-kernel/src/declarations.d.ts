152052829e61631b432d1e1e572e0082f7b8623a
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-08-16 18:55:13 +0900

    Update logger to capture multi-line messages correctly

--- packages/stlite-kernel/src/declarations.d.ts
+++ packages/stlite-kernel/src/declarations.d.ts
@@ -32,4 +32,5 @@ declare let _mainScriptPath: string;
 // Declarations for the `self` object in worker.ts some of whose properties are used to share values with the Python environment.
 interface Window {
   __requirements__: string[];
+  __logCallback__: (msg: string) => void;
 }

e2387e6b966e8ff6274a6b6da3edad97642d886c
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-08-17 20:48:02 +0900

    fix/playground app with requirements (#124)
    * Update playground app to install requirements and fix init script
    
    * Add link to the Streamlit doc
    
    * Update the playground sample to use hiplot

--- packages/playground/src/App.tsx
+++ packages/playground/src/App.tsx
@@ -10,10 +10,40 @@ import { Client as Styletron } from "styletron-engine-atomic";
 import { Provider as StyletronProvider } from "styletron-react";
 const engine = new Styletron({ prefix: "st-" });
 
-const DEFAULT_VALUE = `import streamlit as st
+const DEFAULT_VALUE = `### Sample code copied from https://docs.streamlit.io/library/api-reference/charts/st.pyplot ###
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
-name = st.text_input("Name")
-st.write("Hello ", name or "world")`;
+size = st.slider("Sample size", 100, 1000)
+
+arr = np.random.normal(1, 1, size=size)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
+
+### Sample code copied from https://facebookresearch.github.io/hiplot/tuto_streamlit.html ###
+import json
+import streamlit as st
+import hiplot as hip
+
+x1, x2, x3 = st.slider('x1'), st.slider('x2'), st.slider('x3')
+
+# Create your experiment as usual
+data = [{'uid': 'a', 'dropout': 0.1, 'lr': 0.001, 'loss': 10.0, 'optimizer': 'SGD', 'x': x1},
+        {'uid': 'b', 'dropout': 0.15, 'lr': 0.01, 'loss': 3.5, 'optimizer': 'Adam', 'x': x2},
+        {'uid': 'c', 'dropout': 0.3, 'lr': 0.1, 'loss': 4.5, 'optimizer': 'Adam', 'x': x3}]
+xp = hip.Experiment.from_iterable(data)
+
+# Instead of calling directly \`.display()\`
+# just convert it to a streamlit component with \`.to_streamlit()\` before
+ret_val = xp.to_streamlit(ret="selected_uids", key="hip").display()
+
+st.markdown("hiplot returned " + json.dumps(ret_val))
+`;
+
+const DEFAULT_REQUIREMENTS = ["matplotlib", "hiplot"];
 
 function App() {
   const [mainScriptData, setMainScriptData] = useState(DEFAULT_VALUE);
@@ -23,6 +53,7 @@ function App() {
     const kernel = new StliteKernel({
       pyodideUrl: "https://cdn.jsdelivr.net/pyodide/v0.21.0/full/pyodide.js",
       command: "run",
+      requirements: DEFAULT_REQUIREMENTS,
       mainScriptData,
     });
     setKernel(kernel);
@@ -43,7 +74,16 @@ function App() {
 
   return (
     <>
-      <EditorModal defaultValue={mainScriptData} onChange={setMainScriptData} />
+      <EditorModal
+        defaultValue={mainScriptData}
+        onChange={setMainScriptData}
+        defaultRequirementsValue={DEFAULT_REQUIREMENTS.join("\n")}
+        onInstallRequired={(requirements) => {
+          kernel?.install(requirements).then(() => {
+            console.log("Installed");
+          });
+        }}
+      />
       {kernel && (
         <StliteKernelProvider kernel={kernel}>
           <StyletronProvider value={engine}>
--- packages/playground/src/EditorModal.tsx
+++ packages/playground/src/EditorModal.tsx
@@ -1,4 +1,4 @@
-import { useRef, useCallback } from "react";
+import { useState, useRef, useCallback } from "react";
 import { Rnd } from "react-rnd";
 import Editor, { OnMount } from "@monaco-editor/react";
 
@@ -6,14 +6,59 @@ const DEFAULT_WIDTH = Math.min(600, window.innerWidth);
 const DEFAULT_X = Math.min(50, (window.innerWidth - DEFAULT_WIDTH) / 2);
 const DEFAULT_HEIGHT = Math.min(400, window.innerHeight - 400);
 
+const TABS = ["code", "requirements"] as const;
+
+interface RadioGroupProps<T extends string = string> {
+  name: string;
+  options: readonly T[];
+  value: T;
+  onChange: (value: T) => void;
+}
+function RadioGroup<T extends string = string>({
+  name,
+  onChange,
+  value: currentValue,
+  options,
+}: RadioGroupProps<T>) {
+  const onClick = useCallback<React.ChangeEventHandler<HTMLInputElement>>(
+    (e) => {
+      onChange(e.target.value as T);
+    },
+    [onChange]
+  );
+  return (
+    <>
+      {options.map((value) => (
+        <label className="radio-label">
+          <input
+            type="radio"
+            className="radio-radio"
+            name={name}
+            value={value}
+            checked={value === currentValue}
+            onChange={onClick}
+          />
+          {value}
+        </label>
+      ))}
+    </>
+  );
+}
+
 export interface EditorModalProps {
   defaultValue: string;
   onChange: (value: string) => void;
+  defaultRequirementsValue: string;
+  onInstallRequired: (requirements: string[]) => void;
 }
 function EditorModal(props: EditorModalProps) {
+  const [tab, setTab] = useState<typeof TABS[number]>("code");
+
   const editorRef = useRef<Parameters<OnMount>[0]>(null);
 
-  const { defaultValue, onChange } = props;
+  const { defaultValue, onChange, onInstallRequired } = props;
+
+  const requirementsTextRef = useRef<HTMLTextAreaElement>(null);
 
   return (
     <Rnd
@@ -31,8 +76,10 @@ function EditorModal(props: EditorModalProps) {
       cancel=".editor-container,.editor-modal-footer"
     >
       <div className="editor-modal">
-        <div className="editor-modal-header">Code Editor</div>
-        <div className="editor-container">
+        <div className="editor-modal-header">
+          <RadioGroup name="tab" value={tab} options={TABS} onChange={setTab} />
+        </div>
+        <div className="editor-container" hidden={tab !== "code"}>
           <Editor
             defaultValue={defaultValue}
             defaultLanguage="python"
@@ -41,8 +88,18 @@ function EditorModal(props: EditorModalProps) {
             }, [])}
           />
         </div>
+
+        <div className="editor-container" hidden={tab !== "requirements"}>
+          <textarea
+            className="requirements-textarea"
+            defaultValue={props.defaultRequirementsValue}
+            ref={requirementsTextRef}
+          />
+        </div>
+
         <div className="editor-modal-footer">
           <button
+            hidden={tab !== "code"}
             onClick={useCallback(() => {
               const editor = editorRef.current;
               if (editor == null) {
@@ -53,6 +110,31 @@ function EditorModal(props: EditorModalProps) {
           >
             Save
           </button>
+          <button
+            hidden={tab !== "requirements"}
+            onClick={useCallback(() => {
+              const value = requirementsTextRef.current?.value || "";
+              const requirements = value
+                .split("\n")
+                .map((r) => r.trim())
+                .filter((r) => r !== "");
+              onInstallRequired(requirements);
+            }, [onInstallRequired])}
+          >
+            Install
+          </button>
+
+          <span className="footnote">
+            See{" "}
+            <a
+              href="https://docs.streamlit.io/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              the Streamlit documentation
+            </a>{" "}
+            for the usage of it.
+          </span>
         </div>
       </div>
     </Rnd>

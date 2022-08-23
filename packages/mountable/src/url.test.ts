5c6626b6ec2539b941df5ebdb21c32a6f61337c5
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-08-23 16:46:21 +0900

    Fix tests

--- packages/mountable/src/url.test.ts
+++ packages/mountable/src/url.test.ts
@@ -22,13 +22,20 @@ describe("getParentUrl()", () => {
       expected: "https://whitphx.github.io/stlite/lib/mountable/"
     },
     {
+      // A relative URL is resolved based on window.location.origin.
       input: "./stlite.js",
       expected: "https://example.com:3000/"
     },
     {
+      // A relative URL is resolved based on window.location.origin.
       input: "./stlite/lib/mountable/stlite.js",
       expected: "https://example.com:3000/stlite/lib/mountable/"
     },
+    {
+      // An empty URL is treated as "./"
+      input: "",
+      expected: "https://example.com:3000/"
+    }
   ]
   testCases.forEach(({ input, expected }) => {
     it(`extracts "${expected}" from "${input}"`, () => {

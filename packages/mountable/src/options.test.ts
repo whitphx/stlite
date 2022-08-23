e887ad4364d51153755fcf9a0fe5291d78bc8127
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-08-23 17:30:12 +0900

    Apply Prettier

--- packages/mountable/src/options.test.ts
+++ packages/mountable/src/options.test.ts
@@ -1,4 +1,4 @@
-import { canonicalizeMountOptions } from "./options"
+import { canonicalizeMountOptions } from "./options";
 
 describe("canonicalizeMountOptions()", () => {
   it("translates a string input into StliteKernelOptions", () => {
@@ -7,56 +7,62 @@ describe("canonicalizeMountOptions()", () => {
       entrypoint: "streamlit_app.py",
       files: {
         "streamlit_app.py": {
-          data: "foo"
-        }
+          data: "foo",
+        },
       },
       requirements: [],
-    })
-  })
+    });
+  });
 
   it("fills `command`, `entrypoint`, and `requirements` fields and converts the `files` into the canonical form", () => {
-    expect(canonicalizeMountOptions({
-      files: {
-        "streamlit_app.py": "foo"
-      }
-    })).toEqual({
+    expect(
+      canonicalizeMountOptions({
+        files: {
+          "streamlit_app.py": "foo",
+        },
+      })
+    ).toEqual({
       command: "run",
       entrypoint: "streamlit_app.py",
       requirements: [],
       files: {
         "streamlit_app.py": {
-          data: "foo"
-        }
+          data: "foo",
+        },
       },
-    })
-  })
+    });
+  });
 
   it("preserves the `entrypoint` field if specified", () => {
-    expect(canonicalizeMountOptions({
-      entrypoint: "foo.py",
-      files: {
-        "streamlit_app.py": "foo"
-      }
-    })).toEqual({
+    expect(
+      canonicalizeMountOptions({
+        entrypoint: "foo.py",
+        files: {
+          "streamlit_app.py": "foo",
+        },
+      })
+    ).toEqual({
       command: "run",
       entrypoint: "foo.py",
       requirements: [],
       files: {
         "streamlit_app.py": {
-          data: "foo"
-        }
+          data: "foo",
+        },
       },
-    })
+    });
   });
 
   it("preserves the `requirements` option if specified", () => {
-    expect(canonicalizeMountOptions({
-      requirements: ["matplotlib"]
-    })).toEqual({
+    expect(
+      canonicalizeMountOptions({
+        requirements: ["matplotlib"],
+      })
+    ).toEqual({
       command: "run",
       requirements: ["matplotlib"],
       entrypoint: "streamlit_app.py",
-      files: {}
-    })
-  })
-})
+      files: {},
+    });
+  });
+});

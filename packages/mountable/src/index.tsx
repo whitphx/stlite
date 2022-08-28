d8186b47d80c93782457a2d378ee67a6cb5235e8
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-08-28 18:24:15 +0900

    Add st.latex to the development sample of the mountable package

--- packages/mountable/src/index.tsx
+++ packages/mountable/src/index.tsx
@@ -78,6 +78,12 @@ fig, ax = plt.subplots()
 ax.hist(arr, bins=20)
 
 st.pyplot(fig)
+
+st.latex(r'''
+     a + ar + a r^2 + a r^3 + \\cdots + a r^{n-1} =
+     \\sum_{k=0}^{n-1} ar^k =
+     a \\left(\\frac{1-r^{n}}{1-r}\\right)
+     ''')
     `,
       },
       requirements: ["matplotlib"],

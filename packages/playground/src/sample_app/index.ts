fa03985448a17b7b07c1aba81499d95a9344542b
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-08-31 19:24:50 +0900

    Rename the HiPlot demo and Add the map demo

--- packages/playground/src/sample_app/index.ts
+++ packages/playground/src/sample_app/index.ts
@@ -3,13 +3,15 @@
 import hello_py from "!!raw-loader!./Hello.py"
 import plotting_demo_py from "!!raw-loader!./pages/1_Plotting_Demo.py"
 import matplotlib_demo_py from "!!raw-loader!./pages/Matplotlib_Demo.py"
-import hiplot_demo_py from "!!raw-loader!./pages/Hiplot_Demo.py"
+import map_demo_py from "!!raw-loader!./pages/Map_Demo.py"
+import custom_component_demo_py from "!!raw-loader!./pages/Custom_Component_Demo_(HiPlot).py"
 
 const files: { [path: string]: string } = {
   "Hello.py": hello_py,
   "pages/1_Plotting_Demo.py": plotting_demo_py,
   "pages/Matplotlib_Demo.py": matplotlib_demo_py,
-  "pages/Hiplot_Demo.py": hiplot_demo_py,
+  "pages/Map_Demo.py": map_demo_py,
+  "pages/Custom_Component_Demo_(HiPlot).py.py": custom_component_demo_py,
 }
 
 export default files;

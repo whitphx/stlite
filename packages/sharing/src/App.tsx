ab94e647f8cebcdfdc422ae31cd53d06d3c3bb2d
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-09-08 11:28:36 +0900

    Fix sharing/src/App.tsx

--- packages/sharing/src/App.tsx
+++ packages/sharing/src/App.tsx
@@ -1,7 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { StliteKernel, StliteKernelOptions } from "@stlite/stlite-kernel";
-import { getAppDataFromUrl } from "@stlite/sharing-common";
+import { AppData, getAppDataFromUrl } from "@stlite/sharing-common";
 import StreamlitApp from "./StreamlitApp";
+
+function convertFiles(
+  appDataFiles: AppData["files"]
+): StliteKernelOptions["files"] {
+  let files: StliteKernelOptions["files"] = {};
+  Object.keys(appDataFiles).forEach((key) => {
+    const value = appDataFiles[key];
+    files[key] = {
+      data: value.type === "text" ? value.data : new Uint8Array(value.data),
+    };
+  });
+  return files;
+}
 
 function App() {
   const [kernel, setKernel] = useState<StliteKernel>();
@@ -10,37 +23,26 @@ function App() {
 
     console.debug("Initialize with", appData);
 
-    if (appData == null) {
-      const kernel = new StliteKernel({
-        command: "run",
-        entrypoint: "streamlit_app.py",
-        files: {
-          "streamlit_app.py": {
-            data: `import streamlit as st
+    const kernel = new StliteKernel(
+      appData
+        ? {
+            command: "run",
+            entrypoint: appData.entrypoint,
+            files: convertFiles(appData.files),
+            requirements: appData.requirements,
+          }
+        : {
+            command: "run",
+            entrypoint: "streamlit_app.py",
+            files: {
+              "streamlit_app.py": {
+                data: `import streamlit as st
 st.write("Hello World")`,
-          },
-        },
-        requirements: [],
-      });
-      setKernel(kernel);
-      return;
-    }
-
-    let files: StliteKernelOptions["files"];
-    files = {};
-    Object.keys(appData.files).forEach((key) => {
-      const value = appData.files[key];
-      files[key] = {
-        data: value.type === "text" ? value.data : new Uint8Array(value.data),
-      };
-    });
-
-    const kernel = new StliteKernel({
-      command: "run",
-      entrypoint: appData.entrypoint,
-      files,
-      requirements: appData.requirements,
-    });
+              },
+            },
+            requirements: [],
+          }
+    );
     setKernel(kernel);
 
     return () => {

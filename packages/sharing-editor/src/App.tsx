42985006006e99a592f59b4ff85837f178381c4d
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-09-13 20:59:54 +0900

    Fix the default appData of the editor

--- packages/sharing-editor/src/App.tsx
+++ packages/sharing-editor/src/App.tsx
@@ -11,68 +11,16 @@ import StliteSharingIFrame, {
   StliteSharingIFrameRef,
 } from "./StliteSharingIFrame";
 import Editor, { EditorProps } from "./Editor";
+import { loadDefaultAppData } from "./default-app-data";
 
 const SHARING_APP_URL =
   process.env.REACT_APP_SHARING_APP_URL ?? "http://localhost:3000/";
 const SHARING_APP_ORIGIN = new URL(SHARING_APP_URL).origin;
 
-// NOTE: Only for dev and demo purpose
-const stliteLogoPngUrl = "https://whitphx.github.io/stlite/logo512.png";
-let stliteLogoPng: ArrayBuffer | undefined = undefined;
-const fetchStliteLogoPngUrl = (): Promise<ArrayBuffer> => {
-  if (stliteLogoPng) {
-    return Promise.resolve(stliteLogoPng);
-  }
-
-  return fetch(stliteLogoPngUrl)
-    .then((res) => res.arrayBuffer())
-    .then((binary) => {
-      stliteLogoPng = binary;
-      return stliteLogoPng;
-    });
-};
-
 function App() {
   const [appData, setAppData] = useState<AppData>();
   useEffect(() => {
-    fetchStliteLogoPngUrl().then((binary) => {
-      setAppData({
-        entrypoint: "streamlit_app.py",
-        files: {
-          "streamlit_app.py": {
-            content: {
-              $case: "text",
-              text: `
-import streamlit as st
-from PIL import Image
-
-st.title("Main page")
-
-image = Image.open('data/logo.png')
-st.image(image, caption='Sunrise by the mountains')
-`,
-            },
-          },
-          "pages/page1.py": {
-            content: {
-              $case: "text",
-              text: `
-import streamlit as st
-
-st.title("Sub page")`,
-            },
-          },
-          "data/logo.png": {
-            content: {
-              $case: "data",
-              data: new Uint8Array(binary),
-            },
-          },
-        },
-        requirements: [],
-      });
-    });
-    return;
+    loadDefaultAppData().then(setAppData);
   }, []);
 
   const url = useMemo(

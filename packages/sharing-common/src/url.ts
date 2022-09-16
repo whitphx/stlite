77c8b5c1652e3046738824219ec0b4a4b7a0c7ac
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-09-17 02:54:34 +0900

    Update @stlite/sharing to read the source URL from the URL fragment and load it

--- packages/sharing-common/src/url.ts
+++ packages/sharing-common/src/url.ts
@@ -2,14 +2,37 @@ import { AppData } from "./proto/models";
 import { URL_HASH_PREFIX } from "./consts";
 import { encodeAppData, decodeAppData } from "./compress";
 
-export function extractAppDataFromUrl(): AppData | null {
+export function extractAppDataFromUrl(): Promise<AppData> {
   const hashValue = window.location.hash.replace(/^#/, "");
-  if (!hashValue.startsWith(URL_HASH_PREFIX)) {
-    return null;
+  if (hashValue.startsWith(URL_HASH_PREFIX)) {
+    const encodedAppData = hashValue.slice(1);
+    const appData = decodeAppData(encodedAppData);
+    return Promise.resolve(appData);
   }
 
-  const encodedAppData = hashValue.slice(1);
-  return decodeAppData(encodedAppData);
+  if (hashValue.startsWith("http")) {
+    const url = hashValue;
+    return fetch(url)
+      .then((res) => res.text())
+      .then((content) => {
+        const entrypoint = "app.py"; // TODO
+        const appData: AppData = {
+          entrypoint,
+          files: {
+            [entrypoint]: {
+              content: {
+                $case: "text",
+                text: content,
+              },
+            },
+          },
+          requirements: [],
+        };
+        return appData;
+      });
+  }
+
+  return Promise.reject();
 }
 
 export function embedAppDataToUrl(url: string, appData: AppData): string {

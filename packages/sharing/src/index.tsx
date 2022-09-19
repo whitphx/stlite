2f6cc39dd49e9bab85150ba405fd458270265f49
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-09-19 16:52:30 +0900

    Add toasts on the sharing app

--- packages/sharing/src/index.tsx
+++ packages/sharing/src/index.tsx
@@ -2,10 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// Detect the system color mode Ref: https://stackoverflow.com/a/57795495/13103190
+const isDarkMode =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ToastContainer
+      style={{ zIndex: 999999 }}
+      theme={isDarkMode ? "dark" : "light"}
+    />
   </React.StrictMode>,
   document.getElementById("root") as HTMLElement
 );

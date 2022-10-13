936bb92bbfe98d0c7e29ca7546160b5f2110a0e8
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-10-13 22:32:27 +0900

    Generate the requirements field in the manifest from the requirements.txt file

--- packages/sharing-editor/bin/gen-sample-app-manifests-json.ts
+++ packages/sharing-editor/bin/gen-sample-app-manifests-json.ts
@@ -13,11 +13,11 @@ const jsonOutputPath = path.join(srcDirPath, "./sample-app-manifests.json");
 const SampleAppRawManifest = Record({
   title: String,
   entrypoint: String,
-  requirements: Array(String)
 });
 type SampleAppRawManifest = Static<typeof SampleAppRawManifest>;
 const SampleAppManifest = SampleAppRawManifest.extend({
-  files: Array(String)
+  files: Array(String),
+  requirements: Array(String),
 });
 type SampleAppManifest = Static<typeof SampleAppManifest>;
 
@@ -25,7 +25,7 @@ async function walk(dirPath: string, relative: boolean): Promise<string[]> {
   const filePaths: string[] = [];
   const childNames = await fsPromises.readdir(dirPath);
   await Promise.all(
-    childNames.map(async childName => {
+    childNames.map(async (childName) => {
       const childPath = path.join(dirPath, childName);
       const stat = await fsPromises.stat(childPath);
       if (stat.isDirectory()) {
@@ -41,25 +41,45 @@ async function walk(dirPath: string, relative: boolean): Promise<string[]> {
     return filePaths;
   }
 
-  return filePaths.map(absPath => path.relative(dirPath, absPath));
+  return filePaths.map((absPath) => path.relative(dirPath, absPath));
 }
 
 async function parseManifestAndFiles(
   sampleAppDirName: string
 ): Promise<SampleAppManifest> {
   const sampleAppDirPath = path.join(sampleAppRootDirPath, sampleAppDirName);
+
   const rawManifestFileName = "stlite.json";
   const rawManifestFilePath = path.join(sampleAppDirPath, rawManifestFileName);
   const maybeRawManifest = await fsExtra.readJSON(rawManifestFilePath);
   const rawManifest = SampleAppRawManifest.check(maybeRawManifest);
 
+  const requirementsTextName = "requirements.txt";
+  const requirementsTextPath = path.join(
+    sampleAppDirPath,
+    requirementsTextName
+  );
+  let requirements: string[] = [];
+  try {
+    const requirementsTextData = await fsPromises.readFile(
+      requirementsTextPath,
+      { encoding: "utf-8" }
+    );
+    requirements = requirementsTextData
+      .split("\n")
+      .map((r) => r.trim())
+      .filter((r) => r !== "");
+  } catch {}
+
   const files = (await walk(sampleAppDirPath, true)).filter(
-    fileName => fileName !== rawManifestFileName
+    (fileName) =>
+      fileName !== rawManifestFileName && fileName !== requirementsTextName
   );
 
   return {
     ...rawManifest,
-    files
+    files,
+    requirements,
   };
 }
 
@@ -67,20 +87,20 @@ async function main() {
   // dir names are also considered as the IDs of the sample apps.
   const sampleDirNames = await fsPromises
     .readdir(sampleAppRootDirPath, {
-      withFileTypes: true
+      withFileTypes: true,
     })
-    .then(dirents =>
+    .then((dirents) =>
       dirents
-        .filter(dirent => dirent.isDirectory())
-        .map(dirent => dirent.name)
+        .filter((dirent) => dirent.isDirectory())
+        .map((dirent) => dirent.name)
         .sort()
     );
 
   const sampleAppManifests = await Promise.all(
-    sampleDirNames.map(sampleDirName =>
-      parseManifestAndFiles(sampleDirName).then(manifest => ({
+    sampleDirNames.map((sampleDirName) =>
+      parseManifestAndFiles(sampleDirName).then((manifest) => ({
         id: sampleDirName,
-        ...manifest
+        ...manifest,
       }))
     )
   );

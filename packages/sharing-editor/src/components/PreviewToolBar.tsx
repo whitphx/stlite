bb1d973071880ef7ca74a73e9c75976b273d1b17
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-10-19 19:44:45 +0900

    Set the icon color

--- packages/sharing-editor/src/components/PreviewToolBar.tsx
+++ packages/sharing-editor/src/components/PreviewToolBar.tsx
@@ -63,6 +63,49 @@ function UrlDisplay(props: UrlDisplayProps) {
   );
 }
 
+function ShareIcon() {
+  return (
+    <span className={styles.shareIconContainer}>
+      {/* Set the gradient to the icon. Ref https://github.com/react-icons/react-icons/issues/251#issuecomment-738119416 */}
+      <svg width="0" height="0">
+        <linearGradient
+          id="share-icon-gradient"
+          x1="0"
+          y1="0"
+          x2="100%"
+          y2="100%"
+        >
+          <stop stop-color="rgb(255, 127, 127)" offset="0%" />
+          <stop stop-color="rgb(244, 75, 75)" offset="80%" />
+          <stop stop-color="rgb(173, 74, 82)" offset="100%" />
+        </linearGradient>
+      </svg>
+      <svg viewBox="0 0 32 32" className={styles.shareIconSquare}>
+        <rect
+          width={28}
+          height={28}
+          x={2}
+          y={2}
+          strokeWidth={2}
+          rx={4}
+          ry={4}
+          fill="rgba(0,0,0,0)"
+          stroke="url(#share-icon-gradient)"
+        />
+      </svg>
+      <RiShareLine
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          fill: "url(#share-icon-gradient)",
+        }}
+      />
+    </span>
+  );
+}
+
 interface PreviewToolBarProps {
   sharingUrl: string;
 }
@@ -72,7 +115,7 @@ function PreviewToolBar(props: PreviewToolBarProps) {
       <div className={styles.innerContainer}>
         <div className={styles.card}>
           <div className={styles.cardHeading}>
-            <RiShareLine />
+            <ShareIcon />
           </div>
           <div className={styles.cardItem}>
             <h3 className={styles.cardItemHeading}>Web</h3>

ad7c97f6dc957314ca8983c9b1ec69e99134d93f
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-10-21 18:21:18 +0900

    feature/intro sample app (#369)
    * Add contents on the intro sample
    
    * Add contents
    
    * Rename the sub page
    
    * Fix the title
    
    * Add content

---
+++ packages/sharing-editor/public/samples/000_simple/pages/🎈_subpage.py.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Sub page")
+
+st.markdown("""
+_stlite_ supports **Multi Page Apps (MPA)**, of course!
+
+Create `pages/*.py` like this file to add new pages.
+
+If you are new to MPA, read the official tutorial about it [🔗 here](https://docs.streamlit.io/library/get-started/multipage-apps)
+""")
--- packages/sharing-editor/public/samples/000_simple/streamlit_app.py
+++ packages/sharing-editor/public/samples/000_simple/streamlit_app.py
@@ -1,10 +1,79 @@
 import streamlit as st
 from PIL import Image
 
-st.title("Main page")
+st.title("stlite sharing: Serverless Streamlit app service")
 
 image = Image.open('data/logo.png')
 st.image(image, caption='stlite logo')
 
+st.markdown("""
+### stlite
+**stlite** is a port of _Streamlit_ to Wasm, powered by Pyodide,
+that runs completely on web browsers.
+
+The official repository is [🔗 here](https://github.com/whitphx/stlite).
+
+If you are new to Streamlit, read the Getting Started tutorial [🔗 here](https://docs.streamlit.io/library/get-started) first
+(don't worry, it only takes a few minutes 👍),
+but **you can skip the "Installation" section** because you are here.
+You can start writing code right out of the box on this online editor 😎!
+
+### stlite sharing
+This page is built on **stlite sharing**, an online code editor & sharing platform for _stlite_. \\
+If you see the editor and preview panes side by side, you are on the editor service, https://edit.share.stlite.net/. \\
+If you see only this Streamlit app, you are on the sharing service, https://share.stlite.net/.
+
+The app code and data are encoded into the URL as a hash like `https://share.stlite.net/#!ChBz...`,
+so you can save, share and restore the app only this the URL.
+If you are on the editor page, click the "Open App" link on the top right toolbar to see the standalone app!
+
+You can switch the editor and sharing modes by replacing the host naem in the URL,
+`edit.share.stlite.net` and `share.stlite.net`.
+
+### Tell your story!
+When you create some apps with _stlite_, please share it!
+All you need to do is copy and paste the URL 👍
+
+* **stlite** GitHub Discussions [🔗 here](https://github.com/whitphx/stlite/discussions/categories/show-and-tell)
+* Streamlit community forum [🔗 here](https://discuss.streamlit.io/)
+""")
+
+st.header("Component samples")
+
 name = st.text_input("Your name?")
 st.write("Hello,", name or "world", "!")
+
+value = st.slider("Value?")
+st.write("The slider value is", value)
+
+import numpy as np
+import pandas as pd
+
+st.subheader("Chart sample")
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+tab1, tab2, tab3 = st.tabs(["Line chart", "Area chart", "Bar chart"])
+with tab1:
+    st.line_chart(chart_data)
+with tab2:
+    st.area_chart(chart_data)
+with tab3:
+    st.bar_chart(chart_data)
+
+st.subheader("DataFrame sample")
+df = pd.DataFrame(
+   np.random.randn(50, 20),
+   columns=('col %d' % i for i in range(20)))
+
+st.dataframe(df)
+
+st.subheader("Camera input")
+st.info("Don't worry! The photo data is processed on your browser and never uploaded to any remote servers.")
+enable_camera_input = st.checkbox("Use the camera input")
+if enable_camera_input:
+    picture = st.camera_input("Take a picture")
+
+    if picture:
+        st.image(picture)

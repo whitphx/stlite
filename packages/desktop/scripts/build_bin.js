7ca57b84859e7e61984abd3338c459492cb3a195
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-12-15 11:49:05 +0900

    Unbundle some packages from dump_artifacts.js

--- packages/desktop/scripts/build_bin.js
+++ packages/desktop/scripts/build_bin.js
@@ -12,6 +12,8 @@ require("esbuild")
       "@stlite/kernel", // Don't touch `require.resolve("@stlite/kernel") at the bundle time.
       "pyodide", // The `pyodide` package must be installed at runtime for the included Wasm files, so there is no reason to bundle it here.
       "node-fetch", // `node-fetch` will be installed at runtime anyway because it is one dependency of the `pyodide` package, so there is no reason to bundle it here.
+      "fs-extra", // `fs-extra` and `yargs` will be installed at runtime anyway as the dependencies of `electron-builder`, so we don't have to bundle them here.
+      "yargs", // Same as above, `fs-extra`.
     ],
     outfile: "./bin/dump_artifacts.js",
     logLevel: "info",

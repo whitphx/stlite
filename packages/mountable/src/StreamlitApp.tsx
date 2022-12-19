616b4e3b9352effd4e87f1aaf084248703ed00d4
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-12-19 23:44:21 +0900

    Rename <App /> to <StreamlitApp /> in the mountable package for the consistency compared to other packages

--- packages/mountable/src/StreamlitApp.tsx
+++ packages/mountable/src/StreamlitApp.tsx
@@ -7,10 +7,10 @@ import { Client as Styletron } from "styletron-engine-atomic";
 import { Provider as StyletronProvider } from "styletron-react";
 const engine = new Styletron({ prefix: "st-" });
 
-export interface AppProps {
+export interface StreamlitAppProps {
   kernel: StliteKernel;
 }
-function App(props: AppProps) {
+function StreamlitApp(props: StreamlitAppProps) {
   return (
     <StliteKernelProvider kernel={props.kernel}>
       <StyletronProvider value={engine}>
@@ -20,4 +20,4 @@ function App(props: AppProps) {
   );
 }
 
-export default App;
+export default StreamlitApp;

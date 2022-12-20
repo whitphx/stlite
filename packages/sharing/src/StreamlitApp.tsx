51476c3c49de11b937445477dc437e4078d97d36
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-12-20 10:55:54 +0900

    Fix inconsistent prop name AppProps for <StreamlitApp /> component (#456)

--- packages/sharing/src/StreamlitApp.tsx
+++ packages/sharing/src/StreamlitApp.tsx
@@ -7,10 +7,10 @@ import { Client as Styletron } from "styletron-engine-atomic";
 import { Provider as StyletronProvider } from "styletron-react";
 const engine = new Styletron({ prefix: "st-" });
 
-export interface AppProps {
+export interface StreamlitAppProps {
   kernel: StliteKernel;
 }
-function StreamlitApp(props: AppProps) {
+function StreamlitApp(props: StreamlitAppProps) {
   return (
     <StliteKernelProvider kernel={props.kernel}>
       <StyletronProvider value={engine}>

62428a6d6ce9d9a77af4eedec856db878b9c7d72
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-01-21 03:13:52 +0900

    Update walkRead() to return objects with POSIX paths even on Windows

--- packages/desktop/electron/__tests__/file.test.ts
+++ packages/desktop/electron/__tests__/file.test.ts
@@ -10,17 +10,4 @@ describe("walkRead", () => {
       "bar/bar.txt": Buffer.from("consectetur adipiscing elit\n"),
     });
   });
-
-  it("walks in the specified directory, reads the all child file contents, and returns the pairs of the absolute path and the file content if relative=false", async () => {
-    const targetDirPath = path.resolve(__dirname, "./foo/");
-    const result = await walkRead(targetDirPath, false);
-    expect(result).toEqual({
-      [path.resolve(targetDirPath, "./foo.txt")]: Buffer.from(
-        "Lorem ipsum dolor sit amet\n"
-      ),
-      [path.resolve(targetDirPath, "./bar/bar.txt")]: Buffer.from(
-        "consectetur adipiscing elit\n"
-      ),
-    });
-  });
 });

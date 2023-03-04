8c9b7f4bc3884253da1ba36617fa5be30131310f
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-03-04 22:29:26 +0900

    Update vscode-stlite to load build/index.html

--- packages/vscode-stlite/src/web/extension.ts
+++ packages/vscode-stlite/src/web/extension.ts
@@ -1,12 +1,51 @@
 import * as vscode from 'vscode';
+import * as path from "path";
+
+const WEBVIEW_APP_DIR = "build";  // TODO: Automate copying the `build` directory from `vscode-webview-app` to this package.
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('"vscode-stlite" is activated in the web extension host.');
 
   let disposable = vscode.commands.registerCommand('vscode-stlite.start', () => {
-    const panel = vscode.window.createWebviewPanel('stlite', 'stlite', vscode.ViewColumn.One, { enableScripts: true });
+    const panel = vscode.window.createWebviewPanel(
+      'stlite',
+      'stlite',
+      vscode.ViewColumn.One,
+      {
+        enableScripts: true,
+        localResourceRoots: [
+          vscode.Uri.joinPath(vscode.Uri.file(context.extensionPath), WEBVIEW_APP_DIR)
+        ]
+      }
+    );
 
-    panel.webview.html = getWebviewContent();
+    const indexHtmlUri = vscode.Uri.file(context.asAbsolutePath(path.join(WEBVIEW_APP_DIR, 'index.html')));
+    const assetManifestJsonUri = vscode.Uri.file(context.asAbsolutePath(path.join(WEBVIEW_APP_DIR, 'asset-manifest.json')));
+    const decoder = new TextDecoder();
+
+    Promise.all([
+      vscode.workspace.fs.readFile(indexHtmlUri),
+      vscode.workspace.fs.readFile(assetManifestJsonUri),
+    ]).then(([indexHtmlData, assetManifestJsonData]) => {
+      const indexHtmlText = decoder.decode(indexHtmlData);
+      const assetManifestJson = JSON.parse(decoder.decode(assetManifestJsonData));
+
+      const pathUrlMap = Object.values<string>(assetManifestJson["entrypoints"]).reduce<{ [key: string]: vscode.Uri }>((acc, file) => {
+        const onDiskPath = vscode.Uri.joinPath(context.extensionUri, WEBVIEW_APP_DIR, file);
+        acc[file] = panel.webview.asWebviewUri(onDiskPath);;
+
+        return acc;
+      }, {});
+
+      let updatedIndexHtml = indexHtmlText;
+      Object.entries(pathUrlMap).forEach(([originalPath, newUrl]) => {
+        updatedIndexHtml = updatedIndexHtml.replace("/" + originalPath, newUrl.toString());
+      });
+
+      panel.webview.html = updatedIndexHtml;
+    }, (error) => {
+      console.error(error);
+    });
 
     panel.onDidDispose(() => {
       // Clean up
@@ -18,38 +57,3 @@ export function activate(context: vscode.ExtensionContext) {
 
 // This method is called when your extension is deactivated
 export function deactivate() { }
-
-function getWebviewContent() {
-  return `<!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, shrink-to-fit=no"
-      />
-      <title>stlite app</title>
-      <link
-        rel="stylesheet"
-        href="http://localhost:8000/build/stlite.css"
-      />
-    </head>
-    <body>
-      <div id="root"></div>
-      <script src="http://localhost:8000/build/stlite.js"></script>
-      <script>
-        window.parent = { postMessage: function() { } };
-        stlite.mount(
-          \`
-import streamlit as st
-
-name = st.text_input('Your name')
-st.write("Hello,", name or "world")
-\`,
-          document.getElementById("root")
-        );
-      </script>
-    </body>
-  </html>`;
-}

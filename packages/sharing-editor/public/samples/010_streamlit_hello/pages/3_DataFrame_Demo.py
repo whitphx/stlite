ee1199c51fc1ef55ef7ab461f89086a51b895e73
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-03-20 15:37:01 +0900

    Fix hello demo pages (#517)

--- packages/sharing-editor/public/samples/010_streamlit_hello/pages/3_DataFrame_Demo.py
+++ packages/sharing-editor/public/samples/010_streamlit_hello/pages/3_DataFrame_Demo.py
@@ -1,10 +1,10 @@
-# Copyright 2018-2022 Streamlit Inc.
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,16 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import streamlit as st
-import pandas as pd
-import altair as alt
-from streamlit.hello.utils import show_code
-
 from urllib.error import URLError
+
+import altair as alt
+import pandas as pd
+
+import streamlit as st
+from streamlit.hello.utils import show_code
 
 
 def data_frame_demo():
-    @st.cache
+    @st.cache_data
     def get_UN_data():
         # AWS_BUCKET_URL = "http://streamlit-demo-data.s3-us-west-2.amazonaws.com"
         # df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")

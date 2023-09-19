b6ebcaac7efe483bd5b97c92b3fa96c247bde123
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-09-19 15:10:55 +0900

    Upgrade Pyodide to 0.24.0

--- packages/kernel/src/worker.ts
+++ packages/kernel/src/worker.ts
@@ -58,7 +58,7 @@ async function loadPyodideAndPackages() {
   console.debug("Import the entrypoint script.");
   importScripts(
     pyodideEntrypointUrl ??
-      "https://cdn.jsdelivr.net/pyodide/v0.23.3/full/pyodide.js"
+      "https://cdn.jsdelivr.net/pyodide/v0.24.0/full/pyodide.js"
   );
 
   console.debug("Loading Pyodide");

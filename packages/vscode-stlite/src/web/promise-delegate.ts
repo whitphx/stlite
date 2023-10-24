65341090ac779e3c140643a6c76061ffb7f35ae6
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-10-24 11:59:32 +0900

    Fix PromiseDelegate

--- packages/vscode-stlite/src/web/promise-delegate.ts
+++ packages/vscode-stlite/src/web/promise-delegate.ts
@@ -1,19 +1,15 @@
 type PromiseImplFn<T> = ConstructorParameters<typeof Promise<T>>[0];
 
 export class PromiseDelegate<T> {
-  private promiseInternal: Promise<T>;
+  public readonly promise: Promise<T>;
   private resolveInternal!: Parameters<PromiseImplFn<T>>[0];
   private rejectInternal!: Parameters<PromiseImplFn<T>>[1];
 
   constructor() {
-    this.promiseInternal = new Promise((resolve, reject) => {
+    this.promise = new Promise((resolve, reject) => {
       this.resolveInternal = resolve;
       this.rejectInternal = reject;
     });
-  }
-
-  get promise(): Promise<T> {
-    return this.promiseInternal;
   }
 
   public resolve(value: T): void {

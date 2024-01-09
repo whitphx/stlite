c4109e792efde700ff5bededf75c7c95070ada2b
lukasmasuch <lukas.masuch@gmail.com>
2024-01-09 13:45:00 +0100

    Remove header

--- packages/kernel/src/streamlit-replacements/lib/ConnectionManager.ts
+++ packages/kernel/src/streamlit-replacements/lib/ConnectionManager.ts
@@ -1,21 +1,6 @@
 // Mimic https://github.com/streamlit/streamlit/blob/1.27.0/frontend/app/src/connection/ConnectionManager.ts
 // and WebsocketConnection.
 
-/**
- * Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 import { ReactNode } from "react"
 
 import {
@@ -232,7 +217,7 @@ export class ConnectionManager {
 
   private async connect(): Promise<void> {
     const WEBSOCKET_STREAM_PATH = "_stcore/stream" // The original is defined in streamlit/frontend/src/lib/WebsocketConnection.tsx
-    
+
     try {
       // Stlite Modifications:
       // this.connection = await this.connectToRunningServer()
@@ -240,11 +225,8 @@ export class ConnectionManager {
       this.setConnectionState(ConnectionState.CONNECTED)
     } catch (e) {
       const err = ensureError(e)
-      logError(err.message)
-      this.setConnectionState(
-        ConnectionState.DISCONNECTED_FOREVER,
-        err.message
-      )
+      console.error(err.message)
+      this.setConnectionState(ConnectionState.DISCONNECTED_FOREVER, err.message)
     }
   }
 

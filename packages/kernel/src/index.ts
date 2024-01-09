8e568aa62821f930148475668eabb35bd1f6f140
lukasmasuch <lukas.masuch@gmail.com>
2024-01-09 13:33:31 +0100

    Add back connection manager

--- packages/kernel/src/index.ts
+++ packages/kernel/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./kernel";
+export * from "./streamlit-replacements/lib/ConnectionManager";
 export * from "./react-helpers";
 export * from "./types";
---
+++ packages/kernel/src/streamlit-replacements/lib/ConnectionManager.ts
@@ -0,0 +1,293 @@
+/**
+ * Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { ReactNode } from "react"
+
+import {
+  IHostConfigResponse,
+  BaseUriParts,
+  getPossibleBaseUris,
+  logError,
+  SessionInfo,
+  StreamlitEndpoints,
+  ensureError,
+  BackMsg,
+  ForwardMsg,
+} from "@streamlit/lib"
+
+import { ConnectionState } from "./ConnectionState"
+import { WebsocketConnection } from "./WebsocketConnection"
+
+import type { StliteKernel } from "@stlite/kernel"
+
+interface MessageQueue {
+  [index: number]: any
+}
+
+/**
+ * When the websocket connection retries this many times, we show a dialog
+ * letting the user know we're having problems connecting. This happens
+ * after about 15 seconds as, before the 6th retry, we've set timeouts for
+ * a total of approximately 0.5 + 1 + 2 + 4 + 8 = 15.5 seconds (+/- some
+ * due to jitter).
+ */
+const RETRY_COUNT_FOR_WARNING = 6
+
+interface Props {
+  /**
+   * Kernel object to connect to.
+   */
+  kernel: StliteKernel
+
+  /** The app's SessionInfo instance */
+  sessionInfo: SessionInfo
+
+  /** The app's StreamlitEndpoints instance */
+  endpoints: StreamlitEndpoints
+
+  /**
+   * Function to be called when we receive a message from the server.
+   */
+  onMessage: (message: ForwardMsg) => void
+
+  /**
+   * Function to be called when the connection errors out.
+   */
+  onConnectionError: (errNode: ReactNode) => void
+
+  /**
+   * Called when our ConnectionState is changed.
+   */
+  connectionStateChanged: (connectionState: ConnectionState) => void
+
+  /**
+   * Function to get the auth token set by the host of this app (if in a
+   * relevant deployment scenario).
+   */
+  claimHostAuthToken: () => Promise<string | undefined>
+
+  /**
+   * Function to clear the withHostCommunication hoc's auth token. This should
+   * be called after the promise returned by claimHostAuthToken successfully
+   * resolves.
+   */
+  resetHostAuthToken: () => void
+
+  /**
+   * Function to set the host config for this app (if in a relevant deployment
+   * scenario).
+   */
+  onHostConfigResp: (resp: IHostConfigResponse) => void
+}
+
+/**
+ * Manages our connection to the Server.
+ */
+export class ConnectionManager {
+  private readonly props: Props
+
+  private connection?: WebsocketConnection
+
+  private connectionState: ConnectionState = ConnectionState.INITIAL
+
+  constructor(props: Props) {
+    this.props = props
+
+    this.props.kernel.onWebSocketMessage(payload => {
+      if (typeof payload === "string") {
+        console.error("Unexpected payload type.")
+        return
+      }
+      this.handleMessage(payload)
+    })
+
+    this.props.kernel.loaded.then(() => {
+      console.log("The kernel has been loaded. Start connecting.")
+      this.props.onHostConfigResp(this.props.kernel.hostConfig)
+      this.connect()
+    })
+  }
+
+  /**
+   * Indicates whether we're connected to the server.
+   */
+  public isConnected(): boolean {
+    return this.connectionState === ConnectionState.CONNECTED
+  }
+
+  /**
+   * Return the BaseUriParts for the server we're connected to,
+   * if we are connected to a server.
+   */
+  public getBaseUriParts(): BaseUriParts | undefined {
+    // Stlite Modification:
+    // if (this.connection instanceof WebsocketConnection) {
+    //   return this.connection.getBaseUriParts()
+    // }
+    if (this.connectionState === ConnectionState.CONNECTED) {
+      // The existence of this property became necessary for multi-page apps: https://github.com/streamlit/streamlit/pull/4698/files#diff-e56cb91573ddb6a97ecd071925fe26504bb5a65f921dc64c63e534162950e1ebR967-R975
+      // so here a dummy BaseUriParts object is returned.
+      // The host and port are set as dummy values that are invalid as a URL
+      // in order to avoid unexpected accesses to external resources,
+      // while the basePath is representing the actual info.
+      return {
+        host: "xxx",
+        port: 99999,
+        // When a new session starts, a page name for multi-page apps (a relative path to the app root url) is calculated based on this `basePath`
+        // then a `rerunScript` BackMsg is sent to the server with `pageName` (https://github.com/streamlit/streamlit/blob/ace58bfa3582d4f8e7f281b4dbd266ddd8a32b54/frontend/src/App.tsx#L1064)
+        // and `window.history.pushState` is called (https://github.com/streamlit/streamlit/blob/ace58bfa3582d4f8e7f281b4dbd266ddd8a32b54/frontend/src/App.tsx#L665).
+        basePath: this.props.kernel.basePath,
+      }
+    }
+    return undefined
+  }
+
+  public sendMessage(obj: BackMsg): void {
+    // Stlite Modification:
+    // if (this.connection instanceof WebsocketConnection && this.isConnected()) {
+    //   this.connection.sendMessage(obj)
+    // } else {
+    //   // Don't need to make a big deal out of this. Just print to console.
+    //   logError(`Cannot send message when server is disconnected: ${obj}`)
+    // }
+    if (this.isConnected()) {
+      this.props.kernel.sendWebSocketMessage(
+        BackMsg.encode(BackMsg.create(obj)).finish()
+      )
+    } else {
+      // Don't need to make a big deal out of this. Just print to console.
+      console.error(`Cannot send message when server is disconnected: ${obj}`)
+    }
+  }
+
+  /**
+   * Increment the runCount on our message cache, and clear entries
+   * whose age is greater than the max.
+   */
+  public incrementMessageCacheRunCount(maxMessageAge: number): void {
+    // StaticConnection does not use a MessageCache.
+    if (this.connection instanceof WebsocketConnection) {
+      this.connection.incrementMessageCacheRunCount(maxMessageAge)
+    }
+  }
+
+  private async connect(): Promise<void> {
+    try {
+      // Stlite Modifications:
+      // this.connection = await this.connectToRunningServer()
+      await this.props.kernel.connectWebSocket("/_stcore/stream")
+      this.setConnectionState(ConnectionState.CONNECTED)
+    } catch (e) {
+      const err = ensureError(e)
+      logError(err.message)
+      this.setConnectionState(
+        ConnectionState.DISCONNECTED_FOREVER,
+        err.message
+      )
+    }
+  }
+
+  disconnect(): void {
+    this.connection?.disconnect()
+  }
+
+  private setConnectionState = (
+    connectionState: ConnectionState,
+    errMsg?: string
+  ): void => {
+    if (this.connectionState !== connectionState) {
+      this.connectionState = connectionState
+      this.props.connectionStateChanged(connectionState)
+    }
+
+    if (errMsg || connectionState === ConnectionState.DISCONNECTED_FOREVER) {
+      this.props.onConnectionError(errMsg || "unknown")
+    }
+  }
+
+  private showRetryError = (
+    totalRetries: number,
+    latestError: ReactNode,
+    // The last argument of this function is unused and exists because the
+    // WebsocketConnection.OnRetry type allows a third argument to be set to be
+    // used in tests.
+    _retryTimeout: number
+  ): void => {
+    if (totalRetries === RETRY_COUNT_FOR_WARNING) {
+      this.props.onConnectionError(latestError)
+    }
+  }
+
+  private connectToRunningServer(): WebsocketConnection {
+    const baseUriPartsList = getPossibleBaseUris()
+
+    return new WebsocketConnection({
+      sessionInfo: this.props.sessionInfo,
+      endpoints: this.props.endpoints,
+      baseUriPartsList,
+      onMessage: this.props.onMessage,
+      onConnectionStateChange: this.setConnectionState,
+      onRetry: this.showRetryError,
+      claimHostAuthToken: this.props.claimHostAuthToken,
+      resetHostAuthToken: this.props.resetHostAuthToken,
+      onHostConfigResp: this.props.onHostConfigResp,
+    })
+  }
+
+  // Stlite Modifications:
+  /**
+   * To guarantee packet transmission order, this is the index of the last
+   * dispatched incoming message.
+   */
+  private lastDispatchedMessageIndex = -1
+
+  /**
+   * And this is the index of the next message we receive.
+   */
+  private nextMessageIndex = 0
+
+  /**
+   * This dictionary stores received messages that we haven't sent out yet
+   * (because we're still decoding previous messages)
+   */
+  private readonly messageQueue: MessageQueue = {}
+
+  private async handleMessage(data: ArrayBuffer): Promise<void> {
+    // Assign this message an index.
+    const messageIndex = this.nextMessageIndex
+    this.nextMessageIndex += 1
+
+    const encodedMsg = new Uint8Array(data)
+    const msg = ForwardMsg.decode(encodedMsg)
+
+    // stlite doesn't handle caches.
+    if (msg.type === "refHash") {
+      throw new Error(`Unexpected cache reference message.`)
+    }
+
+    this.messageQueue[messageIndex] = msg
+
+    // Dispatch any pending messages in the queue. This may *not* result
+    // in our just-decoded message being dispatched: if there are other
+    // messages that were received earlier than this one but are being
+    // downloaded, our message won't be sent until they're done.
+    while (this.lastDispatchedMessageIndex + 1 in this.messageQueue) {
+      const dispatchMessageIndex = this.lastDispatchedMessageIndex + 1
+      this.props.onMessage(this.messageQueue[dispatchMessageIndex])
+      delete this.messageQueue[dispatchMessageIndex]
+      this.lastDispatchedMessageIndex = dispatchMessageIndex
+    }
+  }
+}

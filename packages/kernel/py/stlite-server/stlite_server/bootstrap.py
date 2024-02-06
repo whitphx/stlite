5072a3d9709645bc9310db812cedc9ff2f32bcf8
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-02-07 00:22:20 +0900

    Remove the pyodide-http patch because urllib3 started to support Pyodide from 2.2.0

--- packages/kernel/py/stlite-server/stlite_server/bootstrap.py
+++ packages/kernel/py/stlite-server/stlite_server/bootstrap.py
@@ -144,16 +144,6 @@ def _fix_altair():
         logger.error("Failed to fix Altair", exc_info=e)
 
 
-def _fix_requests():
-    try:
-        import pyodide_http  # type: ignore[import]
-
-        pyodide_http.patch_all()  # Patch all libraries
-    except ImportError:
-        # pyodide_http is not installed. No need to do anything.
-        pass
-
-
 def prepare(
     main_script_path: str,
     args: List[str],
@@ -166,7 +156,6 @@ def prepare(
     _fix_sys_path(main_script_path)
     _fix_matplotlib_crash()
     _fix_altair()
-    _fix_requests()
     _fix_sys_argv(main_script_path, args)
     _fix_pydeck_mapbox_api_warning()
     _install_pages_watcher(main_script_path)

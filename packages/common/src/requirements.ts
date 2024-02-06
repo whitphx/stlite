a92372670526acaeb014bf8f6922608dd428b1e6
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-02-07 01:02:27 +0900

    Fix urllib3 version spec

--- packages/common/src/requirements.ts
+++ packages/common/src/requirements.ts
@@ -1,4 +1,4 @@
 // stlite-specific requirements
 export const STLITE_REQUIREMENTS = [
-  "urllib3==2.2.0", // `urllib3` supports Pyodide since 2.2.0. https://urllib3.readthedocs.io/en/stable/reference/contrib/emscripten.html
+  "urllib3>=2.2.0", // `urllib3` supports Pyodide since 2.2.0. https://urllib3.readthedocs.io/en/stable/reference/contrib/emscripten.html
 ];

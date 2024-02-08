6513ab94fb5b55c347cb6894faaf6168aedd324a
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-02-08 13:48:30 +0900

    Sharing Editor with better loading indicator (#707)
    * Fix the editor to use React-Router's data API
    
    * Use `redirect()` helper
    
    * Introduce NProgress to sharing-editor
    
    * Fix the path pattern to match all

--- packages/sharing-editor/src/index.tsx
+++ packages/sharing-editor/src/index.tsx
@@ -2,15 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "./global.scss";
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Root";
+import App, { loader as appLoader } from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    element: <Root />,
+    children: [
+      {
+        path: "*",
+        index: true,
+        loader: appLoader,
+        element: <App />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>,
   document.getElementById("root") as HTMLElement
 );

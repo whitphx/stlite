926e49aff21bd9b812662329899be3e7c0f16f04
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-02-11 16:13:00 +0900

    Fix packages/sharing-editor/public/samples/012_mpa-hello/pages to use https: instead of http:

--- packages/sharing-editor/public/samples/012_mpa-hello/pages/3_📊_DataFrame_Demo.py
+++ packages/sharing-editor/public/samples/012_mpa-hello/pages/3_📊_DataFrame_Demo.py
@@ -23,7 +23,7 @@ from utils import show_code
 def data_frame_demo():
     @st.cache_data
     def get_UN_data():
-        AWS_BUCKET_URL = "http://streamlit-demo-data.s3-us-west-2.amazonaws.com"
+        AWS_BUCKET_URL = "https://streamlit-demo-data.s3-us-west-2.amazonaws.com"
         df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
         return df.set_index("Region")
 

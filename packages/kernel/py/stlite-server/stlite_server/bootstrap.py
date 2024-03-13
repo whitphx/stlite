13106f4d0af0f2b17f271d7afd4d1b875bf67675
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-03-13 14:26:47 +0900

    Remove matplotlib patch

--- packages/kernel/py/stlite-server/stlite_server/bootstrap.py
+++ packages/kernel/py/stlite-server/stlite_server/bootstrap.py
@@ -38,35 +38,6 @@ def _fix_sys_path(main_script_path: str) -> None:
     sys.path.insert(0, os.path.dirname(main_script_path))
 
 
-def _fix_matplotlib_crash() -> None:
-    """Set Matplotlib backend to avoid a crash.
-
-    The default Matplotlib backend crashes Python on OSX when run on a thread
-    that's not the main thread, so here we set a safer backend as a fix.
-    Users can always disable this behavior by setting the config
-    runner.fixMatplotlib = false.
-
-    This fix is OS-independent. We didn't see a good reason to make this
-    Mac-only. Consistency within Streamlit seemed more important.
-    """
-    if config.get_option("runner.fixMatplotlib"):
-        try:
-            # TODO: a better option may be to set
-            #  os.environ["MPLBACKEND"] = "Agg". We'd need to do this towards
-            #  the top of __init__.py, before importing anything that imports
-            #  pandas (which imports matplotlib). Alternately, we could set
-            #  this environment variable in a new entrypoint defined in
-            #  setup.py. Both of these introduce additional trickiness: they
-            #  need to run without consulting streamlit.config.get_option,
-            #  because this would import streamlit, and therefore matplotlib.
-            import matplotlib
-
-            matplotlib.use("Agg")
-        except ImportError:
-            # Matplotlib is not installed. No need to do anything.
-            pass
-
-
 def _fix_sys_argv(main_script_path: str, args: List[str]) -> None:
     """sys.argv needs to exclude streamlit arguments and parameters
     and be set to what a user's script may expect.
@@ -164,7 +135,6 @@ def prepare(
     but omitting the server initialization.
     """
     _fix_sys_path(main_script_path)
-    _fix_matplotlib_crash()
     _fix_altair()
     _fix_requests()
     _fix_sys_argv(main_script_path, args)

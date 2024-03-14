9e9a0fdc1a2dcf8f884d29cc314ee2fde02d7927
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-03-14 23:03:14 +0900

    Fix the Webpack polyfill setting in vscode-stlite (#787)

--- packages/vscode-stlite/webpack.config.js
+++ packages/vscode-stlite/webpack.config.js
@@ -37,6 +37,7 @@ const webExtensionConfig = {
       // see https://webpack.js.org/configuration/resolve/#resolvefallback
       // for the list of Node.js core module polyfills.
       assert: require.resolve("assert"),
+      path: require.resolve("path-browserify"),
     },
   },
   module: {

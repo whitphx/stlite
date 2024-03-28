1df322f6198cba40552b8d4946eec1ec54e90f0b
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-03-29 00:37:21 +0900

    Fix src/worker.ts

--- packages/kernel/src/worker.ts
+++ packages/kernel/src/worker.ts
@@ -1,15 +1,6 @@
 import { bootstrapWorker } from "./worker-runtime";
-import type { OutMessage } from "./types";
 
-interface StliteWorkerContext extends DedicatedWorkerGlobalScope {
-  postMessage(message: OutMessage, transfer: Transferable[]): void;
-  postMessage(message: OutMessage, options?: StructuredSerializeOptions): void;
-}
-
-// Ref: https://v4.webpack.js.org/loaders/worker-loader/#loading-with-worker-loader
-const ctx = self as StliteWorkerContext;
-
-const DEFAULT_PYODIDE_URL =
-  "https://cdn.jsdelivr.net/pyodide/v0.25.0/full/pyodide.js";
-
-self.onmessage = bootstrapWorker(DEFAULT_PYODIDE_URL, ctx.postMessage);
+self.onmessage = bootstrapWorker(
+  "https://cdn.jsdelivr.net/pyodide/v0.25.0/full/pyodide.js",
+  self.postMessage
+);

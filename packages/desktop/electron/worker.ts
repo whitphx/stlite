311bb3cbff5f729e2f7cccf5b89fbb807a4d4e7b
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-03-29 00:18:39 +0900

    Use the worker_thread module to run the worker

---
+++ packages/desktop/electron/worker.ts
@@ -0,0 +1,17 @@
+import { parentPort } from "node:worker_threads";
+import { bootstrapWorker } from "@stlite/kernel/src/worker-runtime";
+
+function postMessage(value: any) {
+  console.debug("[worker thread] postMessage from worker", value);
+  parentPort?.postMessage(value);
+}
+
+const handleMessage = bootstrapWorker(
+  process.env.PYODIDE_URL as string,
+  postMessage
+);
+
+parentPort?.on("message", ({ data, port }) => {
+  console.debug("[worker thread] parentPort.onMessage", { data, port });
+  handleMessage({ data, ports: [port] } as any);
+});

c10fb07dc998d0a1d06b6095055a97985742f75d
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-04-04 20:23:31 +0900

    Add desktop/samples/file-persistence-idbfs

---
+++ packages/desktop/samples/file-persistence-idbfs/streamlit_app/streamlit_app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import datetime
+
+
+st.title("File persistence sample with IndexedDB-based file system")
+
+st.button("Rerun")
+
+if st.button("Clear `/mnt/hello.txt`"):
+    with open("/mnt/hello.txt", "w") as f:
+        f.write("")
+
+now = datetime.datetime.now()
+with open("/mnt/hello.txt", "a") as f:
+    st.write(f"Writing to `/mnt/hello.txt` at `{now}`...")
+    f.write(f"Hello, Streamlit! {now}\n")
+
+with open("/mnt/hello.txt", "r") as f:
+    st.write("Reading from `/mnt/hello.txt`:")
+    st.code(f.read())

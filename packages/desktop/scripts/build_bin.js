d1d067f488d7232e34c3af36dca91d4cbeb21500
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-04-28 02:07:47 +0900

    Bundle yargs and fs-extra into the dump command

--- packages/desktop/scripts/build_bin.js
+++ packages/desktop/scripts/build_bin.js
@@ -17,8 +17,6 @@ require("esbuild")
     external: [
       "pyodide", // The `pyodide` package must be installed at runtime for the included Wasm files, so there is no reason to bundle it here.
       "node-fetch", // `node-fetch` will be installed at runtime anyway because it is one dependency of the `pyodide` package, so there is no reason to bundle it here.
-      "fs-extra", // `fs-extra` and `yargs` will be installed at runtime anyway as the dependencies of `electron-builder`, so we don't have to bundle them here.
-      "yargs", // Same as above, `fs-extra`.
     ],
     define: {
       "process.env.PATH_FROM_SCRIPT_TO_BUILD": JSON.stringify("../build"),

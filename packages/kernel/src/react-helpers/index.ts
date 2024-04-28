80790ee7edd98b9299c9df19dcabba73adfb3730
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-04-28 13:27:21 +0900

    Rename useStliteImageList to useStliteMediaObjects to use both for an array of Image and an array of Subtitle

--- packages/kernel/src/react-helpers/index.ts
+++ packages/kernel/src/react-helpers/index.ts
@@ -1,5 +1,4 @@
 export * from "./StliteKernelProvider";
-export * from "./image-list";
 export * from "./media";
 export * from "./download-button";
 export { default as CustomComponentIFrame } from "./CustomComponentIFrame";
--- packages/kernel/src/react-helpers/media.ts
+++ packages/kernel/src/react-helpers/media.ts
@@ -51,3 +51,74 @@ export function useStliteMediaObjectUrl(rawUrl: string): string {
 
   return url;
 }
+
+/**
+ * Takes an array whose type is Array<{ url?: string | null }>
+ * and overrides the url of each element by loading data from the stlite kernel
+ * for in-memory resources.
+ */
+export function useStliteMediaObjects<T extends { url?: string | null }>(
+  inputMediaObjects: T[]
+) {
+  const [mediaObjects, setMediaObjects] = useState(inputMediaObjects);
+
+  const kernel = useStliteKernel();
+  useEffect(() => {
+    if (kernel == null) {
+      return;
+    }
+
+    let released = false;
+
+    const generatedObjectUrls: string[] = [];
+    const promises = inputMediaObjects.map((obj) => {
+      if (obj.url == null) {
+        return obj;
+      }
+
+      if (!obj.url.startsWith("/media")) {
+        return obj;
+      }
+
+      return kernel
+        .sendHttpRequest({
+          method: "GET",
+          path: obj.url,
+          headers: {},
+          body: "",
+        })
+        .then(({ statusCode, headers, body }) => {
+          if (released) {
+            return obj;
+          }
+          if (statusCode !== 200) {
+            return obj;
+          }
+
+          const blob = new Blob([body], { type: headers.get("Content-Type") });
+          const objectUrl = URL.createObjectURL(blob);
+          generatedObjectUrls.push(objectUrl);
+          return {
+            ...obj,
+            url: objectUrl,
+          };
+        });
+    });
+
+    Promise.all(promises).then((overriddenMediaObjects) => {
+      if (released) {
+        return;
+      }
+      setMediaObjects(overriddenMediaObjects);
+    });
+
+    return () => {
+      released = true;
+      generatedObjectUrls.forEach((objectUrl) => {
+        URL.revokeObjectURL(objectUrl);
+      });
+    };
+  }, [kernel, inputMediaObjects]);
+
+  return mediaObjects;
+}

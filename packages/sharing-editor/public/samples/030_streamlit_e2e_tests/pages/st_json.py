cedadf87477ba12ab88c9fc8206e03dfe53168f3
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-05-27 15:20:04 +0900

    Update the 030_streamlit_e2e_tests sample where some tests have been removed migrating to playwright e2e tests in the upstream

--- packages/sharing-editor/public/samples/030_streamlit_e2e_tests/pages/st_json.py
+++ packages/sharing-editor/public/samples/030_streamlit_e2e_tests/pages/st_json.py
@@ -1,4 +1,4 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022-2024)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,3 +17,5 @@ import streamlit as st
 data = {"foo": "bar"}
 st.json(data)
 st.json(data, expanded=False)
+data = {"Hello     World": "Foo    Bar"}
+st.json(data)

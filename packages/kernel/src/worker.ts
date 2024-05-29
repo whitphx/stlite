30ac3f2d5fc5fa129748fca4b8089db327c9304d
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-05-29 15:41:28 +0900

    Update Pyodide to 0.26.0

--- packages/kernel/src/worker.ts
+++ packages/kernel/src/worker.ts
@@ -1,6 +1,6 @@
 import { startWorkerEnv } from "./worker-runtime";
 
 self.onmessage = startWorkerEnv(
-  "https://cdn.jsdelivr.net/pyodide/v0.25.1/full/pyodide.js",
+  "https://cdn.jsdelivr.net/pyodide/v0.26.0/full/pyodide.js",
   self.postMessage
 );

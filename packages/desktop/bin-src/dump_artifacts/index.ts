4b8b29c9c805e0b00329e4ae175aba5603f0c08e
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-05-29 16:43:07 +0900

    Specify engines.node in desktop/package.json and remove node-fetch

--- packages/desktop/bin-src/dump_artifacts/index.ts
+++ packages/desktop/bin-src/dump_artifacts/index.ts
@@ -5,7 +5,6 @@ import { hideBin } from "yargs/helpers";
 import path from "node:path";
 import fsPromises from "node:fs/promises";
 import fsExtra from "fs-extra";
-import fetch from "node-fetch";
 import { loadPyodide, type PyodideInterface } from "pyodide";
 import { makePyodideUrl } from "./url";
 import { PrebuiltPackagesData } from "./pyodide_packages";
@@ -14,9 +13,6 @@ import { readConfig } from "./config";
 import { validateRequirements, parseRequirementsTxt } from "@stlite/common";
 import { glob } from "glob";
 import { logger } from "./logger";
-
-// @ts-ignore
-global.fetch = fetch; // The global `fetch()` is necessary for micropip.install() to load the remote packages.
 
 const pathFromScriptToBuild =
   process.env.PATH_FROM_SCRIPT_TO_BUILD ?? "../../build";

b595db7250fc11b2231f386eeb63114dd3a1e467
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-06-25 12:09:11 +0900

    Worker E2E test (#986)
    * Create a unit test that launches the worker and run the app
    
    * Update the streamlit submodule to make the testing module work
    
    * Update worker-runtime to be tested
    
    * Rename the test file
    
    * Add *.e2e.ts to the test target
    
    * Add kernel-test make rule and update the CI to run the added E2E test
    
    * Fix
    
    * Add comment
    
    * Use a sample file for the E2E test that caused a bug fixed in #975
    
    * Add sample files for the E2E test that caused a bug fixed in #981
    
    * Set timeout
    
    * Use spy instead of adding an escape hatch on startWorkerEnv()
    
    * Add comments
    
    * Rename the test file
    
    * Fix
    
    * Enable concurrent execution
    
    * Revert "Enable concurrent execution"
    
    This reverts commit 372f3dfe4df0a8794765302d0d6042fc3f9d3fde.
    
    * Clear mock
    
    * Enable concurrent execution
    
    * Remove the maxConcurrency config as it's set as 5 by default
    
    * Refactoring
    
    * Fix

---
+++ packages/kernel/src/worker-runtime.test.ts
@@ -0,0 +1,167 @@
+import path from "node:path";
+import fsPromises from "node:fs/promises";
+import { suite, test, expect, vitest, afterEach } from "vitest";
+import type { PyodideInterface } from "pyodide";
+import { startWorkerEnv, type PostMessageFn } from "./worker-runtime";
+import * as pyodideLoader from "./pyodide-loader";
+import { WorkerInitialData } from "./types";
+import stliteServerWheelUrl from "stlite_server.whl"; // This is an alias configured in vitest.config.ts
+import streamlitWheelUrl from "streamlit.whl"; // This is an alias configured in vitest.config.ts
+
+const pyodideUrl = path.resolve("../../node_modules/pyodide/pyodide.mjs"); // Installed at the Yarn workspace root;
+
+function getWheelInstallPath(wheelImportUrl: string): string {
+  // `wheelImportUrl` is like `/path/to/stlite_server.whl` that is a URL path.
+  // We need to convert it to a local file path so that it can be referred to in the test environment i.e. Node.js.
+  // Also, we need to add `file://` scheme to it so that `micropip.install()` can install it.
+  return "file://" + path.resolve("." + wheelImportUrl);
+}
+
+interface InitializeWorkerEnvOptions {
+  entrypoint: string;
+  files: WorkerInitialData["files"];
+  requirements?: WorkerInitialData["requirements"];
+}
+function initializeWorkerEnv(
+  options: InitializeWorkerEnvOptions,
+): Promise<PyodideInterface> {
+  return new Promise<PyodideInterface>((resolve, reject) => {
+    let pyodide: PyodideInterface;
+
+    // Get the Pyodide instance from the initializer called in the worker by spying on it.
+    const originalInitPyodide = pyodideLoader.initPyodide;
+    const spiedInitPyodide = vitest
+      .spyOn(pyodideLoader, "initPyodide")
+      .mockImplementationOnce(async (...args) => {
+        const loadedPyodide = await originalInitPyodide(...args);
+        pyodide = loadedPyodide;
+        return loadedPyodide;
+      });
+
+    const postMessage: PostMessageFn = (message) => {
+      if (message.type === "event:loaded") {
+        expect(spiedInitPyodide).toHaveBeenCalled();
+        spiedInitPyodide.mockRestore();
+        resolve(pyodide);
+      } else if (message.type === "event:error") {
+        reject(message.data.error);
+      }
+    };
+
+    const onMessage = startWorkerEnv(pyodideUrl, postMessage, undefined);
+
+    // Send the initializer message to the worker.
+    onMessage(
+      new MessageEvent("message", {
+        data: {
+          type: "initData",
+          data: {
+            files: options.files,
+            entrypoint: options.entrypoint,
+            wheels: {
+              stliteServer: getWheelInstallPath(
+                stliteServerWheelUrl as unknown as string,
+              ),
+              streamlit: getWheelInstallPath(
+                streamlitWheelUrl as unknown as string,
+              ),
+            },
+            archives: [],
+            requirements: options.requirements ?? [],
+            moduleAutoLoad: false,
+            prebuiltPackageNames: [],
+          },
+        },
+      }),
+    );
+  });
+}
+
+const TEST_SOURCES: {
+  files: Record<string, string>;
+  entrypoint: string;
+  requirements?: string[];
+}[] = [
+  {
+    entrypoint: "data.column_config.py",
+    requirements: ["faker"],
+    files: {
+      "data.column_config.py": path.resolve(
+        __dirname,
+        "../../sharing-editor/public/samples/011_component_gallery/pages/data.column_config.py",
+      ),
+    },
+  },
+  {
+    entrypoint: "chat.echo.py",
+    files: {
+      "chat.echo.py": path.resolve(
+        __dirname,
+        "../../sharing-editor/public/samples/011_component_gallery/pages/chat.echo.py",
+      ),
+    },
+  },
+  {
+    entrypoint: "media.logo.py",
+    files: {
+      "media.logo.py": path.resolve(
+        __dirname,
+        "../../sharing-editor/public/samples/011_component_gallery/pages/media.logo.py",
+      ),
+      "pages/images/horizontal_red.png": path.resolve(
+        __dirname,
+        "../../sharing-editor/public/samples/011_component_gallery/pages/images/horizontal_red.png",
+      ),
+      "pages/images/icon_red.png": path.resolve(
+        __dirname,
+        "../../sharing-editor/public/samples/011_component_gallery/pages/images/icon_red.png",
+      ),
+    },
+  },
+];
+
+suite("Worker intergration test running an app", async () => {
+  afterEach(() => {
+    vitest.restoreAllMocks();
+  });
+
+  for (const testSource of TEST_SOURCES) {
+    test.concurrent(
+      `Running ${testSource.entrypoint}`,
+      async () => {
+        const files = Object.fromEntries(
+          await Promise.all(
+            Object.entries(testSource.files).map(
+              async ([filename, filepath]) => {
+                const content = await fsPromises.readFile(filepath);
+                return [filename, { data: content }];
+              },
+            ),
+          ),
+        );
+
+        const pyodide = await initializeWorkerEnv({
+          entrypoint: testSource.entrypoint,
+          files,
+          requirements: testSource.requirements,
+        });
+
+        // The code above setting up the worker env is good enough to check if the worker is set up correctly,
+        // but it doesn't check the error occurred inside the Streamlit app running in the worker.
+        // So, we use the code below to test if the Streamlit app runs without any error.
+        await pyodide.runPythonAsync(`
+from streamlit.testing.v1 import AppTest
+
+at = AppTest.from_file("${testSource.entrypoint}")
+
+await at.run()
+
+assert not at.exception, f"Exception occurred: {at.exception}"
+        `);
+      },
+      {
+        timeout: 30 * 1000,
+      },
+    );
+  }
+});
--- packages/kernel/vitest.config.ts
+++ packages/kernel/vitest.config.ts
@@ -1,10 +1,24 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from "vite";
+import path from "node:path";
 
 export default defineConfig({
   test: {
     environment: "jsdom", // We use jsdom because happy-dom does not work well with iframe.
     setupFiles: ["./setupTests.ts"],
   },
+  resolve: {
+    alias: {
+      "stlite_server.whl": path.resolve(
+        __dirname,
+        "./py/stlite-server/dist/stlite_server-0.1.0-py3-none-any.whl",
+      ),
+      "streamlit.whl": path.resolve(
+        __dirname,
+        "./py/streamlit/lib/dist/streamlit-1.35.0-cp312-none-any.whl",
+      ),
+    },
+  },
+  assetsInclude: ["**/*.whl"],
 });

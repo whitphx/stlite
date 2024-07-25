6265953ac22c7fcc6cc855183be2e2772c5a6d90
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-07-25 14:36:28 +0900

    Fix test param ids

--- packages/kernel/py/stlite-server/stlite_server_tests/astmod_test.py
+++ packages/kernel/py/stlite-server/stlite_server_tests/astmod_test.py
@@ -8,7 +8,7 @@ from stlite_server.astmod import patch
 @pytest.mark.parametrize(
     "test_input,expected",
     [
-        (
+        pytest.param(
             """
 import streamlit
 
@@ -19,8 +19,9 @@ import streamlit
 
 await streamlit.write_stream("Hello, world!")
 """,
+            id="basic_streamlit_write_stream",
         ),
-        (
+        pytest.param(
             """
 import streamlit as st
 
@@ -31,8 +32,9 @@ import streamlit as st
 
 await st.write_stream("Hello, world!")
 """,
+            id="streamlit_alias_write_stream",
         ),
-        (
+        pytest.param(
             """
 import streamlit as foo
 
@@ -43,8 +45,9 @@ import streamlit as foo
 
 await foo.write_stream("Hello, world!")
 """,
+            id="custom_streamlit_alias_write_stream",
         ),
-        (
+        pytest.param(
             """
 import streamlit as st
 
@@ -65,8 +68,9 @@ elif False:
 else:
     await st.write_stream("Goodbye, world!")
 """,
+            id="streamlit_write_stream_in_control_flow",
         ),
-        (
+        pytest.param(
             """
 import streamlit as st
 
@@ -83,8 +87,9 @@ while True:
 else:
     await st.write_stream("Goodbye, world!")
 """,
+            id="streamlit_write_stream_in_loop",
         ),
-        (
+        pytest.param(
             """
 import streamlit as st
 
@@ -101,8 +106,9 @@ for _ in range(10):
 else:
     await st.write_stream("Goodbye, world!")
 """,
+            id="streamlit_write_stream_in_for_loop",
         ),
-        (
+        pytest.param(
             """
 import streamlit as st
 
@@ -131,8 +137,9 @@ except:
 finally:
     await st.write_stream("Hello, world!")
 """,
+            id="streamlit_write_stream_in_try_except",
         ),
-        (
+        pytest.param(
             """
 import streamlit as st
 
@@ -157,8 +164,9 @@ except* TypeError:
 finally:
     await st.write_stream("Hello, world!")
 """,
+            id="streamlit_write_stream_in_try_except_star",
         ),
-        (
+        pytest.param(
             """
 import streamlit as st
 
@@ -181,6 +189,7 @@ match x:
     case _:
         await st.write_stream("Hello, world!")
 """,
+            id="streamlit_write_stream_in_match",
         ),
         #         (
         #             """
@@ -228,7 +237,7 @@ def test_not_convert_st_write_stream(test_input):
 @pytest.mark.parametrize(
     "test_input,expected",
     [
-        (
+        pytest.param(
             """
 import asyncio
 import time
@@ -241,8 +250,9 @@ import time
 
 await asyncio.sleep(1)
 """,
+            id="asyncio_sleep_conversion",
         ),
-        (
+        pytest.param(
             """
 import time
 
@@ -254,8 +264,9 @@ import time
 
 await __asyncio__.sleep(1)
 """,
+            id="asyncio_sleep_conversion_without_asyncio_import",
         ),
-        (
+        pytest.param(
             """
 import time
 
@@ -271,8 +282,9 @@ await __asyncio__.sleep(1)
 
 import asyncio
 """,
+            id="asyncio_sleep_conversion_with_asyncio_import_at_end",
         ),
-        (  # Case of from-import
+        pytest.param(  # Case of from-import
             """
 from time import sleep
 
@@ -294,8 +306,9 @@ def sleep(x):
 
 sleep(1)
 """,
+            id="asyncio_sleep_conversion_with_from_import",
         ),
-        (  # Case of from-import with a wildcard
+        pytest.param(  # Case of from-import with a wildcard
             """
 from time import *
 
@@ -317,8 +330,9 @@ def sleep(x):
 
 sleep(1)
 """,
+            id="asyncio_sleep_conversion_with_from_import_wildcard",
         ),
-        (  # Case of import as
+        pytest.param(  # Case of import as
             """
 import time as t
 
@@ -330,8 +344,9 @@ import time as t
 
 await __asyncio__.sleep(1)
 """,
+            id="asyncio_sleep_conversion_with_import_as",
         ),
-        (
+        pytest.param(
             """
 # When `time.sleep` is called in an async function, it can be converted to `await asyncio.sleep` straight away.
 # Test the case with `import time`.
@@ -347,8 +362,9 @@ async def foo():
     import asyncio as __asyncio__
     await __asyncio__.sleep(1)
 """,
+            id="asyncio_sleep_conversion_in_async_function",
         ),
-        (
+        pytest.param(
             # When `time.sleep` is called in an async function, it can be converted to `await asyncio.sleep` straight away.
             # Test the case with `from time import sleep`.
             """
@@ -364,8 +380,9 @@ async def foo():
     import asyncio as __asyncio__
     await __asyncio__.sleep(1)
 """,
+            id="asyncio_sleep_conversion_in_async_function_with_from_import",
         ),
-        (
+        pytest.param(
             """
 from time import sleep
 
@@ -397,8 +414,9 @@ sl(1)
 sl = sleep
 await __asyncio__.sleep(1)
 """,
+            id="asyncio_sleep_conversion_with_dynamic_resolution",
         ),
-        (
+        pytest.param(
             """
 from __future__ import annotations
 import time
@@ -412,8 +430,9 @@ import time
 
 await __asyncio__.sleep(1)
 """,
+            id="asyncio_sleep_conversion_with_future_import",
         ),
-        (
+        pytest.param(
             """
 \"\"\"Docstring
 \"\"\"
@@ -431,8 +450,9 @@ import time
 
 await __asyncio__.sleep(1)
 """,
+            id="asyncio_sleep_conversion_with_future_import_and_docstring",
         ),
-        (
+        pytest.param(
             """
 \"\"\"Docstring
 \"\"\"
@@ -454,6 +474,7 @@ import time
 
 await __asyncio__.sleep(1)
 """,
+            id="asyncio_sleep_conversion_with_future_import_and_multiple_docstrings",
         ),
     ],
 )
@@ -467,30 +488,34 @@ def test_convert_time_sleep_to_asyncio_sleep(test_input, expected):
 @pytest.mark.parametrize(
     "test_input",
     [
-        # When the `sleep` object is not `time.sleep`.
-        """
+        pytest.param(
+            """
 def sleep(x):
     pass
 
 sleep(1)
 """,
-        # When the `sleep` object is not `time.sleep`.
-        """
+            id="custom_sleep_function",
+        ),
+        pytest.param(
+            """
 import third_party_time as time
 
 time.sleep(1)
 """,
-        # When the `sleep` object is called in a lambda function. Lambda can't have await.
-        """
+            id="third_party_time_sleep",
+        ),
+        pytest.param(
+            """
 import time
 
 f = lambda: time.sleep(1)
 f()
 """,
-        # When the `sleep` object is called in a normal function. FunctionDef can't have await.
-        # We should convert sync function to async function. as well in the future,
-        # but leave it as is for now because it's a bit complicated.
-        """
+            id="sleep_occurs_in_lambda",
+        ),
+        pytest.param(
+            """
 import time
 
 def foo():
@@ -498,14 +523,20 @@ def foo():
 
 foo()
 """,
-        """
+            id="sleep_occurs_in_function_as_free_variable",
+        ),
+        pytest.param(
+            """
 from time import sleep
 
 del sleep
 
 sleep(1)
 """,
-        """
+            id="deleted_sleep",
+        ),
+        pytest.param(
+            """
 from time import sleep
 
 if x:
@@ -513,7 +544,10 @@ if x:
 
 sl(1)
 """,
-        """
+            id="non_deterministic_resolution_binding_in_if_clause",
+        ),
+        pytest.param(
+            """
 from time import sleep
 
 def foo():
@@ -526,7 +560,10 @@ for _ in []:
 
 sl(1)
 """,
-        """
+            id="non_deterministic_resolution_binding_in_for_loop",
+        ),
+        pytest.param(
+            """
 from time import sleep
 
 while x:
@@ -536,7 +573,10 @@ while x:
 
 sl(1)
 """,
-        """
+            id="non_deterministic_resolution_binding_in_while_loop",
+        ),
+        pytest.param(
+            """
 from time import sleep
 
 for sleep in [1, 2, 3]:
@@ -544,7 +584,10 @@ for sleep in [1, 2, 3]:
 
 sleep(1)
 """,
-        """
+            id="sleep_bound_in_for_loop",
+        ),
+        pytest.param(
+            """
 from time import sleep
 
 if sleep := True:
@@ -552,14 +595,20 @@ if sleep := True:
 
 sleep(1)
 """,
-        """
+            id="sleep_bound_in_named_expression",
+        ),
+        pytest.param(
+            """
 import time
 
 time.sleep = lambda x: x
 
 time.sleep(1)
 """,
-        """
+            id="sleep_bound_to_lambda",
+        ),
+        pytest.param(
+            """
 from time import sleep
 
 async def foo(sleep):  # The name `sleep` is bound to the function argument, so not converted.
@@ -567,7 +616,10 @@ async def foo(sleep):  # The name `sleep` is bound to the function argument, so 
 
 foo()
 """,
-        """
+            id="sleep_in_code_block_bound_to_param",
+        ),
+        pytest.param(
+            """
 from time import sleep
 
 async def foo():
@@ -576,7 +628,10 @@ async def foo():
 
 foo()
 """,
-        """
+            id="sleep_in_code_block_local_variable",
+        ),
+        pytest.param(
+            """
 from time import sleep
 
 async def foo():
@@ -585,7 +640,10 @@ async def foo():
 
 foo()
 """,
-        """
+            id="sleep_in_code_block_unbound_local_variable",
+        ),
+        pytest.param(
+            """
 from time import sleep
 
 async def foo():
@@ -595,6 +653,8 @@ sleep = 1
 
 foo()
 """,
+            id="sleep_in_code_block_amgiguous_resolution",
+        ),
     ],
 )
 def test_not_convert_sleep(test_input):

c193331e8693f40447b000ceba9dac82c0364032
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-08-04 16:47:36 +0900

    Add st.dataframe with data loaded from a file

--- packages/desktop/samples/basic/app.py
+++ packages/desktop/samples/basic/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
 from matplotlib import pyplot as plt
 
 st.title("Streamlit Sample Desktop App")
@@ -8,6 +9,8 @@ st.write("Hello World")
 
 st.image("https://static.streamlit.io/examples/cat.jpg", caption="Cute cat")
 
+df = pd.read_csv("./data/table.csv")
+st.dataframe(df)
 
 bins = st.slider("Number of bins", min_value=1, max_value=50, value=20)
 arr = np.random.normal(1, 1, size=100)

43c3765d3dd49ff68a8c57434df261e28acfb41b
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-08-11 14:11:23 +0900

    Fix

--- packages/common-react/src/toastify-components/theme.ts
+++ packages/common-react/src/toastify-components/theme.ts
@@ -1,4 +1,5 @@
-import { darkTheme, getDefaultTheme } from "@streamlit/lib";
+import { darkTheme } from "@streamlit/lib/src/theme/themeConfigs";
+import { getDefaultTheme } from "@streamlit/lib/src/theme/utils";
 
 export function isDarkTheme(): boolean {
   return getDefaultTheme().name === darkTheme.name;

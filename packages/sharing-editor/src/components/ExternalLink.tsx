d2790365eb565fbf6f0a8230405874b828a379eb
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-08-13 11:56:07 +0900

    Fix

--- packages/sharing-editor/src/components/ExternalLink.tsx
+++ packages/sharing-editor/src/components/ExternalLink.tsx
@@ -2,12 +2,11 @@ import React from "react";
 import { RiExternalLinkLine } from "react-icons/ri";
 
 function ExternalLink({
-  href,
   children,
   ...restProps
 }: React.AnchorHTMLAttributes<HTMLAnchorElement>) {
   return (
-    <a href={href} {...restProps} target="_blank" rel="noreferrer noopener">
+    <a {...restProps} target="_blank" rel="noreferrer noopener">
       {children}
       <RiExternalLinkLine />
     </a>

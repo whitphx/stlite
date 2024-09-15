b5add905619904dae3da7cc6b240c5f1979ce9b8
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-09-16 00:31:16 +0900

    Update

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,3 @@
+make init
+echo '\nexport REACT_APP_SHARING_APP_URL=https://${CODESPACE_NAME}-3000.app.github.dev/' >> ~/.zshrc
+echo '\nexport REACT_APP_SHARING_APP_URL=https://${CODESPACE_NAME}-3000.app.github.dev/' >> ~/.bashrc

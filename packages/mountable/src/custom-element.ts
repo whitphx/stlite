f7e12c9b0fac2466eb1bd6545ab70cd95e864d5c
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-10-10 22:37:40 +0900

    Fix

--- packages/mountable/src/custom-element.ts
+++ packages/mountable/src/custom-element.ts
@@ -22,7 +22,7 @@ class StliteAppElement extends HTMLElement {
 }
 
 // Deferring the custom element registration until the DOM is ready.
-// If not, `this.innerHTML` will be empty in `connectedCallback`
+// If not, `this.textContent` will be empty in `connectedCallback`
 // because the browser has not parsed the content yet.
 // Using `setTimeout()` is also a solution but it might not be the best practice as written in the article below.
 // Ref: https://dbushell.com/2024/06/15/custom-elements-unconnected-callback/

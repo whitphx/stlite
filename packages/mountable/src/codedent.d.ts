e6f1e4b2e3c6375ba69cc9d461077ab7749caf25
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-10-18 11:05:56 +0900

    Update codedent.d.ts

--- packages/mountable/src/codedent.d.ts
+++ packages/mountable/src/codedent.d.ts
@@ -1,5 +1,10 @@
 // Type definitions for https://www.npmjs.com/package/codedent, https://github.com/WebReflection/codedent
 
 declare module "codedent" {
-  export default function dedent(str: string): string;
+  declare const codedent: (
+    tpl: string | TemplateStringsArray,
+    ...values: any[]
+  ) => string;
+
+  export default codedent;
 }

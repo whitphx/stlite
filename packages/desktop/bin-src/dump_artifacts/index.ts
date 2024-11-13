dc859548c5f8f6f11916e3a4d5e6fdb75bb35e84
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-11-13 19:42:27 -0400

    Update Streamlit to 1.40.1

--- packages/desktop/bin-src/dump_artifacts/index.ts
+++ packages/desktop/bin-src/dump_artifacts/index.ts
@@ -148,7 +148,7 @@ async function installPackages(
   requirements.push(stliteLibWheel);
   const streamlitWheel = await prepareLocalWheel(
     pyodide,
-    path.join(wheelsDir, "streamlit-1.39.0-cp312-none-any.whl"),
+    path.join(wheelsDir, "streamlit-1.40.1-cp312-none-any.whl"),
   );
   requirements.push(streamlitWheel);
 

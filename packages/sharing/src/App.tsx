4cdde9ef16a3c035832a4d35ce71dfe03292d987
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-11-24 13:54:33 +0000

    Make sharing configurable on the shared worker mode

--- packages/sharing/src/App.tsx
+++ packages/sharing/src/App.tsx
@@ -8,6 +8,7 @@ import {
   ModuleAutoLoadSuccessMessage,
 } from "@stlite/sharing-common";
 import StreamlitApp from "./StreamlitApp";
+import { isSharedWorkerMode } from "./urlparams";
 import {
   makeToastKernelCallbacks,
   StliteKernelWithToast,
@@ -88,7 +89,7 @@ st.write("Hello World")`,
           prebuiltPackageNames: [],
           ...makeToastKernelCallbacks(),
           moduleAutoLoad: true,
-          sharedWorkerMode: true,
+          sharedWorkerMode: isSharedWorkerMode(),
         });
         _kernel = kernel;
         setKernel(kernel);

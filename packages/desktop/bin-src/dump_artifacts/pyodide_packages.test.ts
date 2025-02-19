4de9ed4b73b0ca9e5d05407a4d57ba224a6d943a
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-02-19 18:03:54 +0900

    Add E2E test for PrebuiltPackagesDataReader to load package data from the Pyodide package hosted on CDN

---
+++ packages/desktop/bin-src/dump_artifacts/pyodide_packages.test.ts
@@ -0,0 +1,22 @@
+import { describe, it, expect } from "vitest";
+import { PrebuiltPackagesDataReader } from "./pyodide_packages";
+
+describe("PrebuiltPackagesDataReader", () => {
+  it("should be able to read the package info from the remote URL", async () => {
+    const reader = new PrebuiltPackagesDataReader(
+      "https://cdn.jsdelivr.net/pyodide/v0.27.2/full/",
+    );
+    const packageInfo = await reader.getPackageInfoByName("numpy");
+    expect(packageInfo).toEqual(
+      expect.objectContaining({
+        name: "numpy",
+        version: "2.0.2",
+        file_name: "numpy-2.0.2-cp312-cp312-pyodide_2024_0_wasm32.whl",
+        imports: ["numpy"],
+        install_dir: "site",
+        package_type: "package",
+        unvendored_tests: true,
+      }),
+    );
+  });
+});

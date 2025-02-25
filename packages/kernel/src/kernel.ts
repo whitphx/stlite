f2a267645cf1e05985b0fa87bb01c867131f63ba
Branko Dimitrijoski <brankodimitrioski@gmail.com>
2025-02-25 11:26:15 +0100

    Reverting back the changes for code_completion in the kernel

--- packages/kernel/src/kernel.ts
+++ packages/kernel/src/kernel.ts
@@ -342,7 +342,7 @@ export class StliteKernel {
 
   public getCodeCompletion(
     payload: LanguageServerRequestPayload,
-  ): Promise<ReplyMessageLanguageServerCodeCompletion> {
+  ): Promise<ReplyMessageLanguageServerCodeCompletion["data"]> {
     if (!this._workerInitData.languageServer) {
       throw new Error(
         `Language server not loaded, please set languageServer=true to use this method`,
@@ -354,7 +354,7 @@ export class StliteKernel {
         data: payload,
       },
       "reply:language-server:code_completion",
-    ).then((data) => ({ type: "reply:language-server:code_completion", data }));
+    );
   }
 
   /**

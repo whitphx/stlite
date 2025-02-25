af2fe0a1a2259c4800db4b9792b1c1af1dabab50
Branko Dimitrijoski <brankodimitrioski@gmail.com>
2025-02-25 12:58:50 +0100

    Refactored language server service to use the new communication util

--- packages/sharing-editor/src/Editor/LanguageProviders/LanguageServerService.ts
+++ packages/sharing-editor/src/Editor/LanguageProviders/LanguageServerService.ts
@@ -1,50 +1,30 @@
-import type {
-  IDisposable,
-  languages,
-} from "monaco-editor/esm/vs/editor/editor.api";
+import type { IDisposable } from "monaco-editor/esm/vs/editor/editor.api";
 
-import {
-  CodeCompletionRequestMessage,
-  StliteSharingCommunicationService,
-  StliteSharingResponseEvent,
-} from "./StliteSharingCommunicationService";
-import { StliteSharingIFrameRef } from "../../StliteSharingIFrame";
+import type {
+  LanguageServerCodeCompletionMessage,
+  LanguageServerCodeCompletionRequestPayload,
+  LanguageServerCodeCompletionResponse,
+} from "@stlite/sharing-common";
+import { postMessageToStliteSharing } from "../../stlite-sharing-communication";
 
 export class LanguageServerService implements IDisposable {
-  private sharingAppCommunicationService: StliteSharingCommunicationService;
-  constructor(private stliteSharingIframe: StliteSharingIFrameRef | null) {
-    this.sharingAppCommunicationService = new StliteSharingCommunicationService(
-      stliteSharingIframe
-    );
-  }
-
   dispose(): void {
-    this.sharingAppCommunicationService.close();
+    // cleanup and resources
   }
 
+  private getStliteSharingIframe(): HTMLIFrameElement {
+    return document.querySelector(".preview-iframe") as HTMLIFrameElement;
+  }
   autocomplete(
-    payload: CodeCompletionRequestMessage
-  ): Promise<{ items: languages.CompletionItem[] }> {
-    return new Promise((resolve, _reject) => {
-      const handleAutoComplete = (data: {
-        items: languages.CompletionItem[];
-      }) => {
-        this.sharingAppCommunicationService.off(
-          "reply:language-server:code_completion",
-          handleAutoComplete as StliteSharingResponseEvent
-        );
-        resolve(data);
-      };
-
-      this.sharingAppCommunicationService.on(
-        "reply:language-server:code_completion",
-        handleAutoComplete as StliteSharingResponseEvent
-      );
-
-      this.sharingAppCommunicationService.sendMessage({
-        type: "language-server:code_completion",
-        data: payload,
-      });
-    });
+    payload: LanguageServerCodeCompletionRequestPayload,
+  ): Promise<LanguageServerCodeCompletionResponse> {
+    const iframe = this.getStliteSharingIframe();
+    const dto: LanguageServerCodeCompletionMessage = {
+      type: "language-server:code_completion",
+      data: payload,
+    };
+    return postMessageToStliteSharing(iframe, dto, undefined, true).then(
+      (res) => res.data as LanguageServerCodeCompletionResponse,
+    );
   }
 }

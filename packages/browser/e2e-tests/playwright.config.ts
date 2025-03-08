9a67bbeb11576570a1d8feb651b784a4f7ec7bb2
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-03-08 14:09:14 +0900

    Extend the timeout for CI

--- packages/browser/e2e-tests/playwright.config.ts
+++ packages/browser/e2e-tests/playwright.config.ts
@@ -6,6 +6,10 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
+  timeout: process.env.CI ? 60_000 : undefined,
+  expect: {
+    timeout: process.env.CI ? 60_000 : undefined,
+  },
   reporter: "html",
   webServer: [
     {

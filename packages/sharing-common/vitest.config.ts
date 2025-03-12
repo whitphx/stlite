31974a040272ff79b39014fe11e090a682781698
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-03-12 20:39:26 +0900

    Delete typecheck by vitest in sharing-common because typechecking is done at build time

--- packages/sharing-common/vitest.config.ts
+++ packages/sharing-common/vitest.config.ts
@@ -7,8 +7,5 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "jsdom", // stlite-kernel uses jsdom, so use the same here too.
-    typecheck: {
-      enabled: true,
-    },
   },
 });

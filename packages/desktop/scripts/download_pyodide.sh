a4164cdb6cab313eff9892fa2eab3d4195d06eaf
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-05-30 23:27:07 -0700

    Fix

--- packages/desktop/scripts/download_pyodide.sh
+++ packages/desktop/scripts/download_pyodide.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-PYODIDE_VERSION=$(yarn info --json pyodide | jq -r '.children.Version')
+PYODIDE_VERSION=$(node -e 'const { version } = require("pyodide"); console.log(version)')
+echo "Downloading Pyodide $PYODIDE_VERSION"
 curl -L https://github.com/pyodide/pyodide/releases/download/$PYODIDE_VERSION/pyodide-core-$PYODIDE_VERSION.tar.bz2 | tar xj -C ./build --files-from=./pyodide-files.txt

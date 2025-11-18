e4246cc6cb297338aeb01b9c0a82f080ddd4fb47
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-11-19 00:03:04 +0900

    Dev/vite plugin lib assets (#1682)
    * Use vite-plugin-lib-assets instead of vite-plugin-static-copy
    
    * Add changeset
    
    * Fix

---
+++ .changeset/breezy-toys-stare.md
@@ -0,0 +1,5 @@
+---
+"@stlite/browser": patch
+---
+
+Use vite-plugin-lib-assets to handle the wheel file assets
---
+++ packages/browser/src/declarations.d.ts
@@ -0,0 +1,10 @@
+// These module declarations are necessary to resolve the file imports at TS transpile.
+// Ref: https://github.com/jupyterlite/jupyterlite/blob/main/packages/pyolite-kernel/src/declarations.d.ts
+// See also,
+// https://stackoverflow.com/questions/33915930/using-file-loader-with-es6-modules-and-typescript-in-webpack
+// https://stackoverflow.com/questions/61129013/how-to-import-a-file-into-a-react-app-that-uses-create-react-app-as-raw-text
+
+declare module "*.whl" {
+  const res: string;
+  export default res;
+}

6f6edd61d8957d0cdc5e5340089de6cd24645a9a
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-11-25 20:20:25 +0900

    Add a negative look-behind to avoid ReDoS

--- packages/kernel/src/uri-utils.ts
+++ packages/kernel/src/uri-utils.ts
@@ -1,5 +1,7 @@
 // Ref: https://github.com/streamlit/streamlit/blob/1.12.2/frontend/src/lib/UriUtil.ts#L32-L33
-const FINAL_SLASH_RE = /\/+$/;
+// The original implementation was from above,
+// but we improved it inspired by https://codeql.github.com/codeql-query-help/javascript/js-polynomial-redos/
+const FINAL_SLASH_RE = /(?<!\/)\/+$/;
 const INITIAL_SLASH_RE = /^\/+/;
 export function normalizeBasePath(basePath: string): string {
   return basePath.replace(FINAL_SLASH_RE, "").replace(INITIAL_SLASH_RE, "");

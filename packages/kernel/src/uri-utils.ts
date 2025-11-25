540402b3d13a28236d7e89e05dbb45a5880c5fb6
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-11-25 20:22:59 +0900

    Combine regex objects

--- packages/kernel/src/uri-utils.ts
+++ packages/kernel/src/uri-utils.ts
@@ -1,8 +1,7 @@
 // Ref: https://github.com/streamlit/streamlit/blob/1.12.2/frontend/src/lib/UriUtil.ts#L32-L33
 // The original implementation was from above,
 // but we improved it inspired by https://codeql.github.com/codeql-query-help/javascript/js-polynomial-redos/
-const FINAL_SLASH_RE = /(?<!\/)\/+$/;
-const INITIAL_SLASH_RE = /^\/+/;
+const LEADING_AND_TRAILING_SLASH_RE = /^\/+|(?<!\/)\/+$/g;
 export function normalizeBasePath(basePath: string): string {
-  return basePath.replace(FINAL_SLASH_RE, "").replace(INITIAL_SLASH_RE, "");
+  return basePath.replace(LEADING_AND_TRAILING_SLASH_RE, "");
 }

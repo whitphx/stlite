7c8a64c968bb695471e98314af785e5a42424126
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-12-03 15:19:03 +0900

    Dev/kernel worker test (#1760)
    * Update Vitest for tests using web worker
    
    * upadte

--- packages/kernel/src/kernel.test.ts
+++ packages/kernel/src/kernel.test.ts
@@ -14,49 +14,6 @@ suite(
     beforeAll(async () => {
       const worker = new StliteWorker();
 
-      // XXX: Patching `@vitest/web-worker`'s Worker.postMessage() to set `ports` properly.
-      // The upstream fix is https://github.com/vitest-dev/vitest/pull/9078
-      // so this patch can be removed when we upgrade Vitest to a version that includes the fix.
-
-      // Replacement of https://github.com/vitest-dev/vitest/blob/73b54ce2859d34f3847de465efb3f6affda0f8c1/packages/web-worker/src/utils.ts#L21
-      // that is called from https://github.com/vitest-dev/vitest/blob/73b54ce2859d34f3847de465efb3f6affda0f8c1/packages/web-worker/src/utils.ts#L62
-      function createClonedMessageEvent(
-        data: unknown,
-        transferOrOptions:
-          | StructuredSerializeOptions
-          | Transferable[]
-          | undefined,
-      ) {
-        const transfer = Array.isArray(transferOrOptions)
-          ? transferOrOptions
-          : transferOrOptions?.transfer;
-        return new MessageEvent("message", {
-          // data: structuredClone(data, { transfer }),
-          data,
-          origin,
-          ports: (transfer ?? []).filter((t) => t instanceof MessagePort),
-        });
-      }
-
-      // Patching https://github.com/vitest-dev/vitest/blob/73b54ce2859d34f3847de465efb3f6affda0f8c1/packages/web-worker/src/worker.ts#L163
-      function overridePostMessage(
-        this: any, // eslint-disable-line @typescript-eslint/no-explicit-any
-        data: unknown,
-        transferOrOptions:
-          | StructuredSerializeOptions
-          | Transferable[]
-          | undefined,
-      ) {
-        if (this._vw_messageQueue != null) {
-          this._vw_messageQueue.push([data, transferOrOptions]);
-          return;
-        }
-
-        const event = createClonedMessageEvent(data, transferOrOptions);
-        this._vw_workerTarget.dispatchEvent(event);
-      }
-      worker.postMessage = overridePostMessage.bind(worker);
-
       kernel = new StliteKernel({
         worker,
         pyodideUrl,

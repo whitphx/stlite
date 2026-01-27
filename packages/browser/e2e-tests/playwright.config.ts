a6886b4c73c538100f298908a290d701d668dd09
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2026-01-27 16:20:47 +0900

    Extend the timeout for browser E2E tests (#1857)

--- packages/browser/e2e-tests/playwright.config.ts
+++ packages/browser/e2e-tests/playwright.config.ts
@@ -8,8 +8,7 @@ const __dirname = path.dirname(__filename);
 const isCI = !!process.env.CI;
 
 // Stlite tests require longer timeouts due to Pyodide loading time.
-// CI uses longer timeouts for additional buffer.
-const timeout = isCI ? 60_000 : 30_000;
+const timeout = 90_000;
 
 const pagesDir = path.resolve(__dirname, "pages-dist");
 const fileProtocolBaseURL = pathToFileURL(pagesDir).href + "/";
